a3d46c22b85866b2b3c73e541c2a3fde0fa002d7
Ondřej Čertík <ondrej.certik@gmail.com>
2006-08-27 13:12:07 +0000

    demidovich limits tests
    git-svn-id: http://sympy.googlecode.com/svn/trunk@69 a4ce8c80-e919-0410-b93c-d5f14635a724

---
+++ demidovich.py
@@ -0,0 +1,20 @@
+from sym import exp,ln,symbol,infty,rational
+from limits import limit,limitinf
+
+x=symbol("x")
+def sqrt(x):
+    return x**rational(1,2)
+
+def sqrt3(x):
+    return x**rational(1,3)
+
+def testsimpleproblems():
+    assert limitinf((x+1)*(x+2)*(x+3)/x**3,x)==1  #172
+    #assert limitinf((2**(x+1)+3**(x+1))/(2**x+3**x),x)==3  #175
+    assert limitinf(sqrt(x+1)-sqrt(x),x)==0  #179
+    assert limitinf((2*x-3)*(3*x+5)*(4*x-6)/(3*x**3+x-1),x)==8  #Primjer 1
+    assert limitinf(x/sqrt3(x**3+10),x)==1  #Primjer 2
+    assert limitinf((x+1)**2/(x**2+1),x)==1  #181
+    assert limitinf(1000*x/(x**2-1),x)==0  #182
+    assert limitinf((x**2-5*x+1)/(3*x+7),x)==infty  #183
+    assert limitinf((2*x**2-x+3)/(x**3-8*x+5),x)==0  #184
