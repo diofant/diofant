6a3d899eb5413140d0ade0f63130cbd3963abbd2
Ondřej Čertík <ondrej.certik@gmail.com>
2007-02-09 15:01:23 +0000

    refactoring
    git-svn-id: http://sympy.googlecode.com/svn/trunk@158 a4ce8c80-e919-0410-b93c-d5f14635a724

--- sym/__init__.py
+++ sym/__init__.py
@@ -1,7 +1,2 @@
 """Computer Algebra System"""
-from core.basic import basic
-from core.symbol import symbol
-from core.functions import function,sin,cos,tan,exp,ln
-from core.numbers import rational,real,number,infty
-from core.power import pow,pole_error
-from core.add import add,mul,ncmul
+from core import *
---
+++ sym/core/__init__.py
@@ -0,0 +1,6 @@
+from basic import basic
+from symbol import symbol
+from functions import function,sin,cos,tan,exp,ln
+from numbers import rational,real,number,infty
+from power import pow,pole_error
+from add import add,mul,ncmul
