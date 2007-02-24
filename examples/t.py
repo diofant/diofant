67da8f73e335d83005c9667e46f649f2c5d9de84
Ondřej Čertík <ondrej.certik@gmail.com>
2007-02-24 14:07:27 +0000

    add t.py example
    git-svn-id: http://sympy.googlecode.com/svn/trunk@199 a4ce8c80-e919-0410-b93c-d5f14635a724

---
+++ examples/t.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append("..")
+
+from sym import exp,ln,symbol,infty,rational,sin,cos,limit
+
+x=symbol("x") 
+y=symbol("y") 
+
+#print limit((sin(2*x)/x)**(1+x),x,0)
+e=x-1*y*x*y
+print e
+print e.eval()
