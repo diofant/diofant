e96ef8891f7faf5f2df9bfd254c74ef5e7b643e9
Ondřej Čertík <ondrej.certik@gmail.com>
2007-03-02 17:39:37 +0000

    new hashing algorithm implemented
    git-svn-id: http://sympy.googlecode.com/svn/trunk@222 a4ce8c80-e919-0410-b93c-d5f14635a724

--- sym/core/hashing.py
+++ sym/core/hashing.py
@@ -9,37 +9,57 @@ class mhash(object):
 
         print h.value
 
-    mhash.value contains 4 byte hash value, which depends on the order of
+    mhash.value contains the hash value, which depends on the order of
     objects added. The value can be platform independent or dependent as set by
     the "platform_independent" variable.
+
+    algorithm = 0 .... an algorithm used in python, the result is a 4 byte
+                        hash value
+              = 1 .... an algorithm produced by Professor Daniel J. Bernstein
+                see: http://www.partow.net/programming/hashfunctions/index.html
+                        the result is generally a long int hash value
     """
 
     i2p31=2**31
     i2p32=2**32
     platform_independent=False
     #platform_independent=True
+    algorithm=1
 
     def __init__(self):
-        self.value=0x3456
+        if self.algorithm==0:
+            self.value=0x3456
+        elif self.algorithm==1:
+            self.value=5381
 
     def trimlong(self,x):
+        """Trims an unsigned integer"""
         x=x & 0xFFFFFFFFL
         if x>=mhash.i2p31: x-=mhash.i2p32
         return int(x)
 
     def add(self,item):
-        self.value=self.trimlong(1000003*self.value)^item
+        """Adds any integer to the hash value."""
+        if self.algorithm==0:
+            self.value = self.trimlong(1000003*self.value)^item
+        elif self.algorithm==1:
+            self.value = ((self.value << 5) + self.value) + item
 
     def addstr(self,x):
         if self.platform_independent:
             for l in x:
                 self.addint(ord(l))
         else: #faster
-            #hash only last 5 letters
-            self.add(hash(x[-5:]))
-            #self.add(hash(x))
+            if self.algorithm==0:
+                #hash only last 5 letters, because the python algorithm is not
+                #very strong
+                self.add(hash(x[-5:]))
+            elif self.algorithm==1:
+                self.add(hash(x))
 
     def addint(self,x):
+        """We are not using self.add directly for some reason I forget
+        about."""
         self.add(x+3)
 
     def addfloat(self,x):
--- tests/test_str.py
+++ tests/test_str.py
@@ -9,13 +9,18 @@ z = g.Symbol('z')
 w = g.Symbol('w')
 
 def test_poly_str():
+    #if any of these tests fails, it can still be correct, just the terms can
+    #be in a different order. That happens for example when we change the 
+    #hash algorithm. If it is correct, just add another item in the list [] of
+    #correct results.
     assert str((2*x-(7*x**2 - 2) + 3*y)) == "2*x-(7*x^2-2)+3*y"
     assert str(x-y) == "x-y" 
     assert str(2+-x) == "2-x"
     assert str(x-2) == "x-2"
     assert str((x-y-z-w)) == "x-y-z-w"
-    assert str((x-y-z-w).eval()) == "-w-y-z+x"
-    assert str((x-z*y**2*z*w).eval()) == "-z^2*y^2*w+x"
+    assert str((x-y-z-w).eval()) in ["-w-y-z+x","x-w-y-z"]
+    assert str((x-z*y**2*z*w).eval()) in ["-z^2*y^2*w+x", "x-w*y^2*z^2",
+            "-y^2*z^2*w+x"]
 
 def test_bug1():
     e=(x-1*y*x*y)
