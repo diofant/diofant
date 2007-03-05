ace6ce7f28d434192e9247676f699a8242d39bb9
Fabian Pedregosa <fabian@fseoane.net>
2007-03-05 13:36:15 +0000

    documentation
    git-svn-id: http://sympy.googlecode.com/svn/trunk@244 a4ce8c80-e919-0410-b93c-d5f14635a724

--- sym/core/utils.py
+++ sym/core/utils.py
@@ -1,10 +1,13 @@
 def sign(x):
+    """Return the sign of x, that is, 
+    1 if x is positive, 0 if x == 0 and -1 if x is negative
+    """
     if x < 0: return -1
     elif x==0: return 0
     else: return 1
     
 def isnumber(x):
-    """Return true if x is a number. 
+    """Return True if x is a number. 
     """
     from numbers import Number
     return isinstance(x, (Number, int, float, long))
