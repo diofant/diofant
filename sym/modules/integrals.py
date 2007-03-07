065a1c2d91c6c306507ef035d96ceb3549edd850
Ondřej Čertík <ondrej.certik@gmail.com>
2007-03-07 10:51:42 +0000

    integrals fixed
    git-svn-id: http://sympy.googlecode.com/svn/trunk@278 a4ce8c80-e919-0410-b93c-d5f14635a724

--- sym/modules/integrals.py
+++ sym/modules/integrals.py
@@ -38,22 +38,21 @@ class Integral(Basic):
     def __init__(self, a, b, arg, x, evaluated=False):
         "int_a^b arg  dx"
         Basic.__init__(self,evaluated)
-        self.arg=self.sympify(arg).eval()
+        self.arg=self.sympify(arg)
         self.a=self.sympify(a)
         self.b=self.sympify(b)
         assert isinstance(x, Symbol)
         self.x=x
 
     def eval(self):
-        return self
-        #this doesn't work yet:
-        Integral(self.a,self.b,self.arg.eval(),self.x,evaluated=True)
+        if self.evaluated: return self
+        return Integral(self.a,self.b,self.arg,self.x,evaluated=True)
 
     def diff(self,sym):
         if sym==self.x:
             raise IntegralError("Cannot differentiate the integration variable")
         return (self.b.diff(sym)*self.arg.subs(self.x,self.b)-\
-            self.a.diff(sym)*self.arg.subs(self.x,self.a)).eval()
+            self.a.diff(sym)*self.arg.subs(self.x,self.a))
 
     def __str__(self):
         return "int_{%r}^{%r} (%r) d%r"%(self.a,self.b,self.arg,self.x)
@@ -61,7 +60,7 @@ class Integral(Basic):
     def doit(self):
         """Try to do the integral."""
         F=self.primitive_function(self.arg,self.x)
-        return (F.subs(self.x,self.b)-F.subs(self.x,self.a)).eval()
+        return (F.subs(self.x,self.b)-F.subs(self.x,self.a))
 
     def primitive_function(self,f,x):
         """Try to calculate a primitive function to "f(x)".
