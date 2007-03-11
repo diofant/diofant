4eb606e131ed1fcf39d6dad7a4840f4c8a648010
Fabian Pedregosa <fabian@fseoane.net>
2007-03-11 21:14:39 +0000

    tests with new integrate syntax
    git-svn-id: http://sympy.googlecode.com/svn/trunk@399 a4ce8c80-e919-0410-b93c-d5f14635a724

--- examples/substitution.py
+++ examples/substitution.py
@@ -9,6 +9,6 @@ print e
 print e.subs(sympy.cos(x),y)
 print e.subs(sympy.cos(x),y).subs(y,x**2)
 e=1/sympy.log(x)
-e=e.subs(x,sympy.Real(2.71828))
+e=e.subs(x,sympy.Real("2.71828"))
 print e
 print e.evalf()
--- tests/test_integrals.py
+++ tests/test_integrals.py
@@ -3,39 +3,46 @@ sys.path.append(".")
 
 import py
 
-from sympy import Integral, Symbol, IntegralError, log
+from sympy import integrate, Symbol, IntegralError, log, Rational
 
+x=Symbol("x")
+t=Symbol("t")
+a=Symbol("a")
+    
 def test_basics():
-    x=Symbol("x")
-    t=Symbol("t")
-    a=Symbol("a")
     e=(t+1)**2
-    assert Integral(e, (t,0,x)).diff(x)==(1+x)**2
-    assert Integral(e, (t,0,x)).diff(a)==0
-    py.test.raises(IntegralError,"Integral(e,(t,0,x)).diff(t)")
+    assert integrate(e, (t,0,x), evaluate=False).diff(x)==(1+x)**2
+    assert integrate(e, (t,0,x), evaluate=False).diff(a)==0
+    py.test.raises(IntegralError,"integrate(e,(t,0,x), evaluate=False).diff(t)")
 
-    assert Integral(e, (t,a,x)).diff(x)==(1+x)**2
-    assert Integral(e, (t,a,x)).diff(x)!=-(1+x)**2
-    assert Integral(e, (t,x,a)).diff(x)==-(1+x)**2
+    assert integrate(e, (t,a,x), evaluate=False).diff(x)==(1+x)**2
+    assert integrate(e, (t,a,x), evaluate=False).diff(x)!=-(1+x)**2
+    assert integrate(e, (t,x,a), evaluate=False).diff(x)==-(1+x)**2
 
-    assert Integral(t**2, (t,x,2*x)).diff(x)==7*x**2
+    assert integrate(t**2, (t,x,2*x), evaluate=False).diff(x)==7*x**2
 
 def test_integration():
     x=Symbol("x")
     t=Symbol("t")
-    assert Integral(0, (t,0,x)).doit()==0
-    assert Integral(3, (t,0,x)).doit()==3*x
-    assert Integral(t, (t,0,x)).doit()==x**2/2
-    assert Integral(3*t, (t,0,x)).doit()==3*x**2/2
-    assert Integral(3*t**2, (t,0,x)).doit()==x**3
-    assert Integral(-1/t**2, (t,1,x)).doit()==1/x-1
-    assert Integral(1/t, (t,1,x)).doit()==log(x)
+    assert integrate(0, (t,0,x)) == 0
+    assert integrate(3, (t,0,x)) == 3*x
+    assert integrate(t, (t,0,x)) == x**2/2
+    assert integrate(3*t, (t,0,x))== 3*x**2/2
+    assert integrate(3*t**2, (t,0,x)) == x**3
+    assert integrate(-1/t**2, (t,1,x)) == 1/x-1
+    assert integrate(1/t, (t,1,x)) == log(x)
 
-    assert Integral(t**2+5*t-8, (t,0,x)).doit()==x**3/3+5*x**2/2-8*x
+    assert integrate(t**2+5*t-8, (t,0,x))==x**3/3+5*x**2/2-8*x
 
     a=Symbol("a")
     b=Symbol("b")
     c=Symbol("c")
-    assert Integral(a*t, (t,0,x)).doit()==a*x**2/2
-    assert Integral(a*t**4, (t,0,x)).doit()==a*x**5/5
-    assert Integral(a*t**2+b*t+c, (t,0,x)).doit()==a*x**3/3+b*x**2/2+c*x
+    assert integrate(a*t, (t,0,x))==a*x**2/2
+    assert integrate(a*t**4, (t,0,x))==a*x**5/5
+    assert integrate(a*t**2+b*t+c, (t,0,x))==a*x**3/3+b*x**2/2+c*x
+
+def test_multiple_integration():
+    y = Symbol('y')
+    assert integrate((x**2)*(y**2), (x,0,1), (y,-1,2)) == Rational(1)
+    assert integrate((y**2)*(x**2), x, y) == Rational(1,9)*(x**3)*(y**3)
+    
