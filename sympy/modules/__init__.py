6c917f9a11c343a1421951ba416067c1f4c3c5b9
Fabian Pedregosa <fabian@fseoane.net>
2007-03-12 18:59:06 +0000

    import ispoly
    git-svn-id: http://sympy.googlecode.com/svn/trunk@420 a4ce8c80-e919-0410-b93c-d5f14635a724

--- sympy/modules/__init__.py
+++ sympy/modules/__init__.py
@@ -15,5 +15,6 @@ G{packagetree sympy.modules}
 from sympy.modules.trigonometric import sin, cos, tan, arctan
 from sympy.modules.limits import limit, limitinf, Limit
 from sympy.modules.integrals import integrate, IntegralError
+from sympy.modules.polynomials import ispoly
 from sympy.modules.matrices import Matrix, zero, one, gamma, sigma, \
     minkowski_tensor
