5d40d7b2cd680c7b006d1cf9d854beb8cb2c4585
Ondřej Čertík <ondrej.certik@gmail.com>
2007-03-30 20:44:42 +0000

    solvers improved
    git-svn-id: http://sympy.googlecode.com/svn/trunk@675 a4ce8c80-e919-0410-b93c-d5f14635a724

--- sympy/modules/solvers.py
+++ sympy/modules/solvers.py
@@ -13,18 +13,38 @@ def solve(eq, vars):
       3/2
 
     """
+    def wo(di, x):
+        """Are all items in the dictionary "di" without "x"?"""
+        for d in di:
+            if di[d].has(x):
+                return False
+        return True
+
     #currently solve only for one variable
     if len(vars) == 1:
         x = vars[0]
         a,b,c = [Symbol(s, dummy = True) for s in ["a","b","c"]]
+
         r = eq.match(a*x + b, [a,b])
-        if r: return solve_linear(r[a], r[b])
-        r = eq.match(a*x**2 + b*x +c, [a,b,c])
-        if r: return solve_quadratic(r[a], r[b], r[c])
+        if r and wo(r,x): return solve_linear(r[a], r[b])
+
+        r = eq.match(a*x**2 + c, [a,c])
+        if r and wo(r,x): return solve_quadratic(r[a], 0, r[c])
+
+        r = eq.match(a*x**2 + b*x + c, [a,b,c])
+        if r and wo(r,x): return solve_quadratic(r[a], r[b], r[c])
+
     raise "sorry"
 
 def solve_linear(a, b):
     return -b/a
 
 def solve_quadratic(a, b, c):
-    return a+b
+    D = b**2-4*a*c
+    if D == 0:
+        return [-b/(2*a)]
+    else:
+        return [
+                (-b+D.sqrt())/(2*a),
+                (-b-D.sqrt())/(2*a)
+               ]
--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -5,5 +5,7 @@ from sympy import Rational, Symbol, cos, solve
 
 def test_lienar():
     x = Symbol("x")
-    eq = 3*x-2
-    solve(eq, x)
+    assert solve(3*x-2, x) == Rational(2,3)
+    assert solve(x**2-1, x) == [1, -1]
+    assert solve(((x-1)*(x-2)).expand(), x) in [[1,2], [2,1]]
+    assert solve(((x-1)*(x-1)).expand(), x) == [1]
