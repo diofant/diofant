dbdcf19f3cfd3a6f5f8c93a6899c71ef4f64e6ad
Ondřej Čertík <ondrej.certik@gmail.com>
2007-03-31 00:12:20 +0000

    put Fabian's patch from issue 6 as a comment.
    git-svn-id: http://sympy.googlecode.com/svn/trunk@683 a4ce8c80-e919-0410-b93c-d5f14635a724

--- sympy/modules/limits.py
+++ sympy/modules/limits.py
@@ -187,6 +187,7 @@ def limitinf(e,x):
     elif sig==-1: 
         s.infty.sig=sign(c0,x)
         return s.infty #e0<0: lim f = +-infty   (the sign depends on the sign of c0)
+#        return sign(c0, x) * s.infty #e0<0: lim f = +-infty   (the sign depends on the sign of c0)
     elif sig==0: return limitinf(c0,x) #e0=0: lim f = lim c0
 
 @memoize
