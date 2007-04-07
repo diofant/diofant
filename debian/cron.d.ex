d3d50764a9627843f1c13f434e1ef6c0bb077b1d
Ondřej Čertík <ondrej.certik@gmail.com>
2007-04-07 20:48:36 +0000

    make the deb package lintian conforming
    git-svn-id: http://sympy.googlecode.com/svn/trunk@695 a4ce8c80-e919-0410-b93c-d5f14635a724

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the sympy package
-#
-0 4	* * *	root	sympy_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(sympy):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="sympy" command="/usr/bin/sympy"
