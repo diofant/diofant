84d6d2ee40a64240fc0508593318fb0f7c13305d
Fabian Pedregosa <fabian@fseoane.net>
2007-04-23 21:01:53 +0000

    tests actually didn't get executed ...
    git-svn-id: http://sympy.googlecode.com/svn/trunk@902 a4ce8c80-e919-0410-b93c-d5f14635a724

--- tests/test_trigonometric.py
+++ tests/test_trigonometric.py
@@ -2,14 +2,15 @@ import sys
 sys.path.append(".")
 
 from sympy import *
+from decimal import Decimal
 
 def test_cos():
-    cos(pi) == -1
-    float(cos(1)) == 0.54030230586813977
-    cos(1).evalf() == 0.540302305868171952183697906453
-    float(cos(1) + cos(2)) == 0.12415546932099736
-    float(cos(1)*cos(2)*cos(3)) == 0.22259495730990295
+    assert cos(pi) == -1
+    assert float(cos(1)) == 0.54030230586813977
+    assert cos(1).evalf() == Decimal("0.540302305868171952183697906453")
+    assert float(cos(1) + cos(2)) == 0.12415546932099736
+    assert float(cos(1)*cos(2)*cos(3)) == 0.22259495730990297
 
 def test_sin():
-    sin(pi) == 0
-    float(sin(1)) == 0.8414709848078965
+    assert sin(pi) == 0
+    assert float(sin(1)) == 0.8414709848078965
