c8367a50fef912b913821467974b667fd301940f
Fabian Pedregosa <fabian@fseoane.net>
2007-04-28 21:35:06 +0000

    more tests
    git-svn-id: http://sympy.googlecode.com/svn/trunk@946 a4ce8c80-e919-0410-b93c-d5f14635a724

--- tests/test_latex.py
+++ tests/test_latex.py
@@ -16,6 +16,9 @@ def test_latex_basic():
 def test_latex_functions():
     assert latex(exp(x)) == "${e}^{x}$"
     
-def test_latex_modules():
+def test_latex_integrals():
     assert latex(integrate(log(x), x, evaluate=False)) == "$\int log(x)\,dx$"
+    assert latex(integrate(x**2, (x,0,1), evaluate=False)) == "$\int^0_1 {x}^{2}\,dx$"
+    
+def test_latex_limits():
     assert latex(limit(x, x, oo, evaluate=False)) == "$\lim_{x \to \infty}x$"
