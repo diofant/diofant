108a2b5d158210f0600c18bb5c48b6971dc30c02
Fabian Pedregosa <fabian@fseoane.net>
2007-04-28 22:14:57 +0000

    pretty tests
    git-svn-id: http://sympy.googlecode.com/svn/trunk@949 a4ce8c80-e919-0410-b93c-d5f14635a724

---
+++ tests/test_pretty.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append(".")
+
+import py
+
+from sympy import *
+from sympy.modules.printing import pretty
+
+x = Symbol('x')
+
+def test_pretty_basic():
+    assert pretty( (x**2) ) == ' 2\nx '
+    assert pretty( (x**2 + x + 1)) in ['     2\n1+x+x ']
+    assert pretty( oo ) == "oo"
+
+    
+def test_pretty_functions():
+    assert pretty( (2*x + exp(x)) ) in ['     x\n2*x+e ',' x    \ne +2*x']
+    assert pretty( sqrt(2) ) == '    ___\n  \\/ 2 '
+    assert pretty( sqrt(2+pi) ) == '    ______\n \\/ 2+pi  '
+    
+def test_pretty_integrals():
+    f = integrate(log(x), x, evaluate=False)
+    assert pretty( f ) in ['/          \n| log(x) dx\n/          ']
+    
+def test_pretty_limits():
+    assert pretty( limit(x, x, oo, evaluate=False) ) == ' lim  x\nx->oo  '
+    
