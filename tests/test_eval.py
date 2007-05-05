68bf896d99bc65a06e6856f68dd508b66fd3e39c
Fabian Pedregosa <fabian@fseoane.net>
2007-05-05 14:27:12 +0000

    Cosmetic changes
    git-svn-id: http://sympy.googlecode.com/svn/trunk@1014 a4ce8c80-e919-0410-b93c-d5f14635a724

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -4,50 +4,69 @@ sys.path.append(".")
 import sympy as g
 from sympy import *
 
-def test_eval():
-    a=g.Symbol("a")
-    b=g.Symbol("b")
-    e=a+b+a+b
-    s1=str(e)
+def test_add_eval():
+    a = Symbol("a")
+    b = Symbol("b")
+    
+    e = a+b+a+b
+    s1 = str(e)
     e.eval()
-    s2=str(e)
+    s2 = str(e)
     assert s1 == s2
 
     c=g.Rational(1)
     p=g.Rational(5)
+    
     e=a*b+c+p
     assert e == a*b+6
-    e=c+a+p
+    
+    e = c+a+p
     assert e == a+6
-    e=c+a-p
+    
+    e = c+a-p
     assert e == a+(-4)
-    e=c+a+b*c+a-p
-    assert e == 2*a+b+(-4)
-    e=a+a
+    
+    e = a+a
     assert e == 2*a
+    
     e=a+p+a
     assert e == 2*a+5
-    e=a*g.Rational(2)+p+a
-    assert e == a*2+5+a
-    e=a*g.Rational(2)+p+a
-    assert e == 3*a+5
-    e=c+p
+    
+    e = c+p
     assert e == g.Rational(6)
-    e=a*g.Rational(2)+a
-    assert e == 3*a
+    
     e=b+a-b
     assert e == a
+    
+def test_addmul_eval():
+    
+    e = c+a+b*c+a-p
+    assert e == 2*a+b+(-4)
+    
+    e=a*g.Rational(2)+p+a
+    assert e == a*2+5+a
+    
+    e=a*g.Rational(2)+p+a
+    assert e == 3*a+5
+    
+    e = a*g.Rational(2)+a
+    assert e == 3*a
+    
+    
+def test_mulpow_eval():
+    x = Symbol('x')
+    
+    assert sqrt(50)/(sqrt(2)*x) == 5/x
+    assert sqrt(-2) == I*sqrt(2)
+
 
 def test_evalpow_bug():
-    x=g.Symbol("x")
-    e=1/(1/x)
-    assert e==x
+    x = Symbol("x")
+    
+    assert 1/(1/x) == x
+    
     e=1/(-1/x)
     assert e==-x
-    
-def test_mul_eval():
-    x = Symbol('x')
-    assert sqrt(50)/(sqrt(2)*x) == 5/x
 
 
 def test_expbug():
