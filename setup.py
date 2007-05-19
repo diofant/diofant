74c6d75957023ed7c3d4f220ff10b803e746ac9f
Ondřej Čertík <ondrej.certik@gmail.com>
2007-05-19 22:21:18 +0000

    removed the debian specific things from the setup.py
    git-svn-id: http://sympy.googlecode.com/svn/trunk@1102 a4ce8c80-e919-0410-b93c-d5f14635a724

--- setup.py
+++ setup.py
@@ -40,137 +40,6 @@ if sys.version_info[1] < 4:
           sys.version_info[:2]
     sys.exit(-1)
 
-        
-class bdist_dpkg(Command):
-    """Make a nice .deb package
-    """
-    
-    description = "Make a deb package using dpkg (debuild)"
-    user_options = []  # distutils complains if this is not here.
-
-    def initialize_options(self):  # distutils wants this
-        pass
-    
-    def finalize_options(self):    # this too
-        pass
-
-    def run(self):
-        """
-        debian/changelog contains a version like this:
-
-        0.4~pre+svn739-1
-
-        This method parses it, then checkouts the svn revision as directed (739
-        in this example), but applies the current top svn debian dir to it, and
-        executes "debuild" in that temporary directory.
-        """
-        import os
-        def get_changelog_version_revision():
-            """Reads the first line in changelog, parses 0.4~pre+svn739-1 and
-            returns ("0.4~pre",739,1)
-            """
-            l = file("debian/changelog").readline()
-            import re
-            m = re.match("sympy \((\S+)\+svn(\d+)\-(\d+)\) ",l)
-            if m:
-                g = m.groups()
-                if len(g) == 3:
-                    #version, svn revision, debian revision
-                    #('0.4~pre', '739', '1') 
-                    v, r, dr = g
-                    return v, int(r), int(dr)
-            print l
-            raise "Don't understant the syntax in changelog"
-        version,revision,drevision = get_changelog_version_revision()
-        os.system("mkdir -p dist")
-        tmpdir = "sympy-%s+svn%d" % (version, revision)
-        print "exporting svn (%d) to dist/%s" % (revision,tmpdir)
-        os.system("svn -q export -r %d " % revision +
-                "http://sympy.googlecode.com/svn/trunk/ dist/%s" % tmpdir)  
-        os.system("rm -rf dist/%s/debian" % tmpdir)
-        print "creating dist/sympy_%s+svn%d.orig.tar.gz" % (version, revision)
-        os.system("cd dist; tar zcf sympy_%s+svn%d.orig.tar.gz %s" \
-                %(version, revision, tmpdir))
-        print "creating the deb package"
-        os.system("cp -a debian dist/%s/debian" % tmpdir)
-        os.system("rm -rf dist/%s/debian/.svn" % tmpdir)
-        #os.system("cd dist/%s; debuild -sa -us -uc" % tmpdir)
-        os.system("cd dist/%s; debuild" % tmpdir)
-        os.system("rm -rf dist/%s" % tmpdir)
-        print "-"*50
-        print "Done. Files genereated in the dist/ directory"
-    
-    def run_old(self):
-        """
-        Copies the current local svn copy to the dist/sympy-svn739,
-        creates dist/sympy_0.4-pre+svn739.orig.tar.gz from that and then
-        a debian package.
-
-        debian/changelog contains a version like this:
-
-        0.4-pre+svn739-1
-
-        where the last "-1" is a debian version and this is the only place that
-        contains this number. The "0.4-pre" should be the same as
-        sympy.__init__ and svn739 should be the same as the current svn number.
-
-        This method checks that 0.4-pre+svn739 in changelog is consistent,
-        otherwise refuses to continue.
-        """
-        import os
-        def get_revision():
-            """Returns the current svn revision number as an int."""
-            fin, fout = os.popen2("svn --non-interactive info | grep Revision | cut -c 11-")
-            rev = fout.readlines()
-            return int(rev[0])
-        def get_changelog_version_revision():
-            """Reads the first line in changelog, parses 0.4-pre+svn739-1 and
-            returns ("0.4-pre",739,1)
-            """
-            l = file("debian/changelog").readline()
-            import re
-            m = re.match("sympy \((\S+)\+svn(\d+)\-(\d+)\) ",l)
-            if m:
-                g = m.groups()
-                if len(g) == 3:
-                    #version, svn revision, debian revision
-                    #('0.4-pre', '739', '1') 
-                    v, r, dr = g
-                    return v, int(r), int(dr)
-            print l
-            raise "Don't understant the syntax in changelog"
-        v,r,dr = get_changelog_version_revision()
-        revision=get_revision()
-        version = sympy.__version__
-        pos = version.find("-")
-        if pos != -1:
-            #change "-" to "~" in the Debian package name run this to see why:
-#$ dpkg --compare-versions 0.4-pre+svn756-1 lt 0.4 && echo true || echo false
-#$ dpkg --compare-versions 0.4~pre+svn756-1 lt 0.4 && echo true || echo false
-#$ dpkg --compare-versions 0.3+svn756-1 lt 0.4 && echo true || echo false
-            version = version[:pos]+"~"+version[pos+1:]
-        if version != v or revision != r:
-            print "The version/revision in debian/changelog (%s-svn%d) is " \
-                "inconsistent\nwith the sympy.__version__ (%s) and svn " \
-                "revision (%d)" % (v, r, sympy.__version__, revision)
-            return
-        os.system("mkdir -p dist")
-        tmpdir = "sympy-%s+svn%d" % (version, revision)
-        print "exporting svn to dist/%s" % tmpdir
-        os.system("svn -q export . dist/%s" % tmpdir)  
-        os.system("rm -rf dist/%s/debian" % tmpdir)
-        print "creating dist/sympy_%s+svn%d.orig.tar.gz" \
-                % (version, revision)
-        os.system("cd dist; tar zcf sympy_%s+svn%d.orig.tar.gz %s" \
-                %(version, revision, tmpdir))
-        print "creating the deb package"
-        os.system("cp -a debian dist/%s/debian" % tmpdir)
-        os.system("rm -rf dist/%s/debian/.svn" % tmpdir)
-        #os.system("cd dist/%s; debuild -sa -us -uc" % tmpdir)
-        os.system("cd dist/%s; debuild" % tmpdir)
-        os.system("rm -rf dist/%s" % tmpdir)
-        print "-"*50
-        print "Done. Files genereated in the dist/ directory"
 
 class clean(Command):
     """Cleans *.pyc and debian trashs, so you should get the same copy as 
@@ -282,43 +151,6 @@ class test_sympy(Command):
         tdoc = test_sympy_doc(self.args)
         tdoc.run() # run also the doc test suite
 
-class test_sympy_dpkg(Command):
-    
-    description = "Run tests for the deb package"
-    user_options = []  # distutils complains if this is not here.
-    
-    def initialize_options(self):  # distutils wants this
-        pass
-    
-    def finalize_options(self):    # this too
-        pass
-    
-    def run(self):
-        import os
-        from glob import glob
-        g = glob("dist/*.changes")
-        assert len(g) == 1
-        changes = g[0]
-        g = glob("dist/*.dsc")
-        assert len(g) == 1
-        dsc = g[0]
-        g = glob("dist/*.deb")
-        assert len(g) == 1
-        deb = g[0]
-        print "testing this package:"
-        print "  ",dsc
-        print "  ",changes
-        print "  ",deb
-        print
-        print "running lintian & linda..."
-        os.system("lintian -i %s" % changes)
-        os.system("linda -i %s" % changes)
-        print 'running pbuilder (please run "sudo pbuilder update" yourself)...'
-        os.system("sudo pbuilder build %s" % dsc)
-        print "running piuparts"
-        os.system("sudo piuparts -p %s" % deb)
-        print "Done, see the above output for any errors."
-        
 class test_sympy_doc(Command):
     
     description = "Run the tests for the examples in the documentation"
@@ -395,10 +227,7 @@ setup(
       cmdclass    = {'test': test_sympy, 
                      'test_core' : test_sympy_core,
                      'test_doc' : test_sympy_doc,
-                     'bdist_dpkg' : bdist_dpkg, 
-                     'test_dpkg' : test_sympy_dpkg,
                      'gen_doc' : gen_doc,
                      'clean' : clean, 
                      },
       )
-
