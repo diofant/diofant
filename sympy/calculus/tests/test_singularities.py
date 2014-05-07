03c207fc9fa310d45f2a28a117e972e9585b2a43
Sergey B Kirpichev <skirpichev@gmail.com>
2014-05-07 13:05:35 +0400

    A better implementation of singularities()

--- sympy/calculus/tests/test_singularities.py
+++ sympy/calculus/tests/test_singularities.py
@@ -1,19 +1,18 @@
-from sympy import Symbol, exp, log
+from sympy import Symbol, exp, log, sign
 from sympy.calculus.singularities import singularities
 
-from sympy.utilities.pytest import XFAIL
+from sympy.abc import x
 
 
 def test_singularities():
-    x = Symbol('x', extended_real=True)
+    assert singularities(x**2, x) == set()
+    assert singularities(x/(x**2 + 3*x + 2), x) == {-2, -1}
+    assert singularities(1/(1 + x), x) == {-1}
+    assert singularities(sign(x), x) == {0}
 
-    assert singularities(x**2, x) == ()
-    assert singularities(x/(x**2 + 3*x + 2), x) == (-2, -1)
 
-
-@XFAIL
 def test_singularities_non_rational():
-    x = Symbol('x', extended_real=True)
-
-    assert singularities(exp(1/x), x) == (0)
-    assert singularities(log((x - 2)**2), x) == (2)
+    assert singularities(exp(1/x), x) == {0}
+    assert singularities(log((x - 2)**2), x) == {2}
+    assert singularities(exp(1/x) + log(x + 1), x) == {-1, 0}
+    assert singularities(exp(1/log(x + 1)), x) == {0}
