5a96bb88858aa218c8bfd0f0391be035eed24910
Sergey B Kirpichev <skirpichev@gmail.com>
2015-03-04 14:53:13 +0300

    Set zoo.is_complex to True and zoo.is_real to False

--- sympy/core/numbers.py
+++ sympy/core/numbers.py
@@ -2886,6 +2886,8 @@ class ComplexInfinity(with_metaclass(Singleton, AtomicExpr)):
     is_infinite = True
     is_number = True
     is_prime = False
+    is_complex = True
+    is_real = False
 
     __slots__ = []
 
