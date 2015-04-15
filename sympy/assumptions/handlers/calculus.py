diofant/diofant

commit 50ed6502b3397b7e1d155ffee0272b08aa498204

Reformat certain "Example" headings with bad formatting
Following smichr's advice, I'm replacing the various improperly formatted
"Example" headings to be plural, have no colon, and be underscored by
`=========`.

sympy/assumptions/handlers/calculus.py
@@ -79,7 +79,8 @@ class AskFiniteHandler(CommonHandler):
         """
         Handles Symbol.
 
-        Examples:
+        Examples
+        ========
 
         >>> from sympy import Symbol, Q
         >>> from sympy.assumptions.handlers.calculus import AskFiniteHandler

sympy/core/rules.py
@@ -17,7 +17,7 @@ class Transform(object):
         If supplied, specifies which objects are in the mapping.
 
     Examples
-    --------
+    ========
 
     >>> from sympy.core.rules import Transform
     >>> from sympy.abc import x

sympy/matrices/dense.py
@@ -994,7 +994,7 @@ def symarray(prefix, shape):  # pragma: no cover
       more than one dimension the shape must be a tuple.
 
     Examples
-    --------
+    ========
     These doctests require numpy.
 
     >>> from sympy import symarray

sympy/polys/rings.py
@@ -37,7 +37,7 @@ def ring(symbols, domain, order=lex):
     order : :class:`~sympy.polys.polyoptions.Order` or coercible, optional, defaults to ``lex``
 
     Examples
-    --------
+    ========
 
     >>> from sympy.polys.rings import ring
     >>> from sympy.polys.domains import ZZ
@@ -66,7 +66,7 @@ def xring(symbols, domain, order=lex):
     order : :class:`~sympy.polys.polyoptions.Order` or coercible, optional, defaults to ``lex``
 
     Examples
-    --------
+    ========
 
     >>> from sympy.polys.rings import xring
     >>> from sympy.polys.domains import ZZ
@@ -95,7 +95,7 @@ def vring(symbols, domain, order=lex):
     order : :class:`~sympy.polys.polyoptions.Order` or coercible, optional, defaults to ``lex``
 
     Examples
-    --------
+    ========
 
     >>> from sympy.polys.rings import vring
     >>> from sympy.polys.domains import ZZ
@@ -124,7 +124,7 @@ def sring(exprs, *symbols, **options):
     options : keyword arguments understood by :class:`~sympy.polys.polyoptions.Options`
 
     Examples
-    --------
+    ========
 
     >>> from sympy.core import symbols
     >>> from sympy.polys.rings import sring
@@ -444,8 +444,8 @@ class PolyRing(DefaultPrinting, IPolys):
         """
         Add a sequence of polynomials or containers of polynomials.
 
-        Example
-        -------
+        Examples
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -471,8 +471,8 @@ class PolyRing(DefaultPrinting, IPolys):
         """
         Multiply a sequence of polynomials or containers of polynomials.
 
-        Example
-        -------
+        Examples
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -542,7 +542,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         can copy the polynomial. This method makes a shallow copy.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.domains import ZZ
         >>> from sympy.polys.rings import ring
@@ -610,7 +610,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         """Equality test for polynomials.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.domains import ZZ
         >>> from sympy.polys.rings import ring
@@ -866,7 +866,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         """Add two polynomials.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.domains import ZZ
         >>> from sympy.polys.rings import ring
@@ -940,7 +940,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         """Subtract polynomial p2 from p1.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.domains import ZZ
         >>> from sympy.polys.rings import ring
@@ -994,7 +994,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         """n - p1 with n convertible to the coefficient domain.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.domains import ZZ
         >>> from sympy.polys.rings import ring
@@ -1021,7 +1021,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         """Multiply two polynomials.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.domains import QQ
         >>> from sympy.polys.rings import ring
@@ -1071,7 +1071,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         """p2 * p1 with p2 in the coefficient domain of p1.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.domains import ZZ
         >>> from sympy.polys.rings import ring
@@ -1100,7 +1100,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         """raise polynomial to power `n`
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.domains import ZZ
         >>> from sympy.polys.rings import ring
@@ -1186,7 +1186,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         """square of a polynomial
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1344,7 +1344,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         All polynomials are required not to be Laurent polynomials.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1475,7 +1475,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         mc is a tuple, (monom, coeff), where monomial is (i0, i1, ...)
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1519,7 +1519,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         mc is a tuple, (monom, coeff), where monomial is (i0, i1, ...)
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1605,7 +1605,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         """Leading monomial tuple according to the monomial ordering.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1633,7 +1633,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         element : PolyElement (with ``is_monomial = True``) or 1
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1681,7 +1681,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         Leading monomial as a polynomial element.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1709,7 +1709,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         """Leading term as a polynomial element.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1744,7 +1744,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         order : :class:`~sympy.polys.polyoptions.Order` or coercible, optional
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1769,7 +1769,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         order : :class:`~sympy.polys.polyoptions.Order` or coercible, optional
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1794,7 +1794,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         order : :class:`~sympy.polys.polyoptions.Order` or coercible, optional
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1841,7 +1841,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         else multiply not inplace
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -2138,7 +2138,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         Cancel common factors in a rational function ``f/g``.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys import ring, ZZ
         >>> R, x,y = ring("x,y", ZZ)
@@ -2194,7 +2194,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         """Computes partial derivative in ``x``.
 
         Examples
-        --------
+        ========
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ

sympy/printing/codeprinter.py
@@ -38,7 +38,7 @@ class Assignment(Relational):
         the dimensions will not align.
 
     Examples
-    --------
+    ========
 
     >>> from sympy import symbols, MatrixSymbol, Matrix
     >>> from sympy.printing.codeprinter import Assignment

sympy/simplify/cse_main.py
@@ -154,7 +154,7 @@ def opt_cse(exprs, order='canonical'):
         The expression substitutions which can be useful to optimize CSE.
 
     Examples
-    --------
+    ========
 
     >>> from sympy.simplify.cse_main import opt_cse
     >>> from sympy.abc import x

sympy/strategies/tree.py
@@ -16,7 +16,7 @@ def treeapply(tree, join, leaf=identity):
     Keys are containers/iterables.  Values are functions [a] -> a.
 
     Examples
-    --------
+    ========
 
     >>> from sympy.strategies.tree import treeapply
     >>> tree = [(3, 2), (4, 1)]
@@ -71,8 +71,8 @@ def greedy(tree, objective=identity, **kwargs):
     "Better" is determined by the objective keyword.  This function makes
     choices to minimize the objective.  It defaults to the identity.
 
-    Example
-    -------
+    Examples
+    ========
 
     >>> from sympy.strategies.tree import greedy
     >>> inc    = lambda x: x + 1

sympy/utilities/autowrap.py
@@ -824,7 +824,7 @@ def ufuncify(args, expr, language=None, backend='numpy', tempdir=None,
     [1] http://docs.scipy.org/doc/numpy/reference/ufuncs.html
 
     Examples
-    --------
+    ========
 
     >>> from sympy.utilities.autowrap import ufuncify
     >>> from sympy.abc import x, y

commit 73487721385204edc205224edaf0995d3202b892

Fix wrong heading level for Examples sections
(broken by 50ed650)

sympy/assumptions/handlers/calculus.py
@@ -61,7 +61,8 @@ class AskFiniteHandler(CommonHandler):
 
     Test that an expression is finite respect to all its variables.
 
-    Examples of usage:
+    Examples
+    ========
 
     >>> from sympy import Symbol, Q
     >>> from sympy.assumptions.handlers.calculus import AskFiniteHandler

sympy/core/rules.py
@@ -17,7 +17,7 @@ class Transform(object):
         If supplied, specifies which objects are in the mapping.
 
     Examples
-    ========
+    --------
 
     >>> from sympy.core.rules import Transform
     >>> from sympy.abc import x

sympy/matrices/dense.py
@@ -994,7 +994,8 @@ def symarray(prefix, shape):  # pragma: no cover
       more than one dimension the shape must be a tuple.
 
     Examples
-    ========
+    --------
+
     These doctests require numpy.
 
     >>> from sympy import symarray

sympy/polys/rings.py
@@ -37,7 +37,7 @@ def ring(symbols, domain, order=lex):
     order : :class:`~sympy.polys.polyoptions.Order` or coercible, optional, defaults to ``lex``
 
     Examples
-    ========
+    --------
 
     >>> from sympy.polys.rings import ring
     >>> from sympy.polys.domains import ZZ
@@ -66,7 +66,7 @@ def xring(symbols, domain, order=lex):
     order : :class:`~sympy.polys.polyoptions.Order` or coercible, optional, defaults to ``lex``
 
     Examples
-    ========
+    --------
 
     >>> from sympy.polys.rings import xring
     >>> from sympy.polys.domains import ZZ
@@ -95,7 +95,7 @@ def vring(symbols, domain, order=lex):
     order : :class:`~sympy.polys.polyoptions.Order` or coercible, optional, defaults to ``lex``
 
     Examples
-    ========
+    --------
 
     >>> from sympy.polys.rings import vring
     >>> from sympy.polys.domains import ZZ
@@ -124,7 +124,7 @@ def sring(exprs, *symbols, **options):
     options : keyword arguments understood by :class:`~sympy.polys.polyoptions.Options`
 
     Examples
-    ========
+    --------
 
     >>> from sympy.core import symbols
     >>> from sympy.polys.rings import sring
@@ -1633,7 +1633,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         element : PolyElement (with ``is_monomial = True``) or 1
 
         Examples
-        ========
+        --------
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1744,7 +1744,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         order : :class:`~sympy.polys.polyoptions.Order` or coercible, optional
 
         Examples
-        ========
+        --------
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1769,7 +1769,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         order : :class:`~sympy.polys.polyoptions.Order` or coercible, optional
 
         Examples
-        ========
+        --------
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ
@@ -1794,7 +1794,7 @@ class PolyElement(DomainElement, DefaultPrinting, CantSympify, dict):
         order : :class:`~sympy.polys.polyoptions.Order` or coercible, optional
 
         Examples
-        ========
+        --------
 
         >>> from sympy.polys.rings import ring
         >>> from sympy.polys.domains import ZZ

sympy/printing/codeprinter.py
@@ -38,7 +38,7 @@ class Assignment(Relational):
         the dimensions will not align.
 
     Examples
-    ========
+    --------
 
     >>> from sympy import symbols, MatrixSymbol, Matrix
     >>> from sympy.printing.codeprinter import Assignment

sympy/simplify/cse_main.py
@@ -154,7 +154,7 @@ def opt_cse(exprs, order='canonical'):
         The expression substitutions which can be useful to optimize CSE.
 
     Examples
-    ========
+    --------
 
     >>> from sympy.simplify.cse_main import opt_cse
     >>> from sympy.abc import x

sympy/strategies/tree.py
@@ -72,7 +72,7 @@ def greedy(tree, objective=identity, **kwargs):
     choices to minimize the objective.  It defaults to the identity.
 
     Examples
-    ========
+    --------
 
     >>> from sympy.strategies.tree import greedy
     >>> inc    = lambda x: x + 1

sympy/utilities/autowrap.py
@@ -824,7 +824,7 @@ def ufuncify(args, expr, language=None, backend='numpy', tempdir=None,
     [1] http://docs.scipy.org/doc/numpy/reference/ufuncs.html
 
     Examples
-    ========
+    --------
 
     >>> from sympy.utilities.autowrap import ufuncify
     >>> from sympy.abc import x, y

sympy/utilities/lambdify.py
@@ -553,7 +553,7 @@ def _imp_namespace(expr, namespace=None):
        function
 
     Examples
-    ========
+    --------
 
     >>> from sympy.abc import x
     >>> from sympy.utilities.lambdify import implemented_function, _imp_namespace
@@ -623,7 +623,7 @@ def implemented_function(symfunc, implementation):
        function with attached implementation
 
     Examples
-    ========
+    --------
 
     >>> from sympy.abc import x
     >>> from sympy.utilities.lambdify import lambdify, implemented_function