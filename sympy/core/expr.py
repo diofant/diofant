54b69dc71d28e56e46cd68f02b47ad422354d89b
Sergey B Kirpichev <skirpichev@gmail.com>
2015-07-22 14:44:18 +0300

    fix Expr.__lt__/__le__/etc

--- sympy/core/expr.py
+++ sympy/core/expr.py
@@ -241,6 +241,8 @@ class Expr(Basic, EvalfMixin):
             if me is S.NaN:
                 raise TypeError("Invalid NaN comparison")
         if self.is_extended_real and other.is_extended_real:
+            if self == other:
+                return S.true
             dif = self - other
             if dif.is_nonnegative is not None and \
                     dif.is_nonnegative is not dif.is_negative:
@@ -259,6 +261,8 @@ class Expr(Basic, EvalfMixin):
             if me is S.NaN:
                 raise TypeError("Invalid NaN comparison")
         if self.is_extended_real and other.is_extended_real:
+            if self == other:
+                return S.true
             dif = self - other
             if dif.is_nonpositive is not None and \
                     dif.is_nonpositive is not dif.is_positive:
@@ -277,6 +281,8 @@ class Expr(Basic, EvalfMixin):
             if me is S.NaN:
                 raise TypeError("Invalid NaN comparison")
         if self.is_extended_real and other.is_extended_real:
+            if self == other:
+                return S.false
             dif = self - other
             if dif.is_positive is not None and \
                     dif.is_positive is not dif.is_nonpositive:
@@ -296,6 +302,8 @@ class Expr(Basic, EvalfMixin):
                 raise TypeError("Invalid NaN comparison")
         if self.is_extended_real and other.is_extended_real:
             dif = self - other
+            if self == other:
+                return S.false
             if dif.is_negative is not None and \
                     dif.is_negative is not dif.is_nonnegative:
                 return sympify(dif.is_negative)
