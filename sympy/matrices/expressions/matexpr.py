ddefdd9fe28f6b50919a5121f162eed2c2cf92cb
Keval Shah <kevalshah_96@yahoo.co.in>
2015-08-15 02:19:04 +0530

    Fix matrix power of identity/zero matrices
    Closes sympy/sympy#9823
    
    Signed-off-by: Sergey B Kirpichev <skirpichev@gmail.com>

--- sympy/matrices/expressions/matexpr.py
+++ sympy/matrices/expressions/matexpr.py
@@ -106,7 +106,9 @@ class MatrixExpr(Basic):
     def __pow__(self, other):
         if not self.is_square:
             raise ShapeError("Power of non-square matrix %s" % self)
-        if other is S.NegativeOne:
+        elif self.is_Identity:
+            return self
+        elif other is S.NegativeOne:
             return Inverse(self)
         elif other is S.Zero:
             return Identity(self.rows)
@@ -462,6 +464,8 @@ class ZeroMatrix(MatrixExpr):
             raise ShapeError("Power of non-square matrix %s" % self)
         if other == 0:
             return Identity(self.rows)
+        if other < 1:
+            raise ValueError("Matrix det == 0; not invertible.")
         return self
 
     def _eval_transpose(self):
