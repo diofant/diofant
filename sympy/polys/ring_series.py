sympy/sympy

fixed small bug in `rs_log`

sympy/polys/ring_series.py
@@ -933,9 +933,9 @@ def rs_log(p, x, prec):
     """
     if rs_is_puiseux(p, x):
         return rs_puiseux(rs_log, p, x, prec)
-    if p == 1:
-        return 0
     R = p.ring
+    if p == 1:
+        return R.zero
     if _has_constant_term(p, x):
         const = 0
         zm = R.zero_monom

diofant/diofant

sympy/polys/ring_series.py
@@ -451,7 +451,7 @@ def rs_log(p, x, prec):
     """
     ring = p.ring
     if p == 1:
-        return 0
+        return ring.zero
     if _has_constant_term(p - 1, x):
         raise NotImplementedError('p - 1 must not have a constant term in the series variables')
     dlog = p.diff(x)