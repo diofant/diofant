a39cbaf22401c466f02e5b12e3ebdd46fa8eef0c
Sergey B Kirpichev <skirpichev@gmail.com>
2015-09-30 19:39:29 +0300

    Fix issue refs in test_numpy_piecewise_regression
        [skip ci]

--- sympy/printing/tests/test_numpy.py
+++ sympy/printing/tests/test_numpy.py
@@ -7,7 +7,7 @@ def test_numpy_piecewise_regression():
     """
     NumPyPrinter needs to print Piecewise()'s choicelist as a list to avoid
     breaking compatibility with numpy 1.8. This is not necessary in numpy 1.9+.
-    See gh-9747 and gh-9749 for details.
+    See sympy/sympy#9747 and sympy/sympy#9749 for details.
     """
     p = Piecewise((1, x < 0), (0, True))
     assert NumPyPrinter().doprint(p) == 'select([x < 0,True], [1,0], default=nan)'
