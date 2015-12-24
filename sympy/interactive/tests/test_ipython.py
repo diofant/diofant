f5f1853b02be021317f688f1d673de45abc11470
Sergey B Kirpichev <skirpichev@gmail.com>
2015-12-24 03:36:41 +0300

    Replace ugly hack for wrapping int with Integer in the IPython
    Before was:
    
    In [1]: 1
    Out[1]: 1
    
    In [2]: type(_)
    Out[2]: sympy.core.numbers.One
    
    In [3]: %history
    Integer (1 ) <- bah!
    type (_ ) <- even more, no change should be here at all...
    %history
    
    Right way is using ast transformations.

--- sympy/interactive/tests/test_ipython.py
+++ sympy/interactive/tests/test_ipython.py
@@ -1,8 +1,10 @@
 """Tests of tools for setting up interactive IPython sessions. """
 
-from sympy.interactive.session import (init_ipython_session,
-    enable_automatic_symbols, enable_automatic_int_sympification)
+import ast
 
+from sympy.interactive.session import (init_ipython_session,
+                                       enable_automatic_symbols,
+                                       IntegerWrapper)
 from sympy.core import Symbol, Rational, Integer
 from sympy.external import import_module
 
@@ -12,6 +14,29 @@ readline = import_module("readline")
 if not ipython:
     # py.test will not execute any tests now
     disabled = True
+
+
+def test_IntegerWrapper():
+    tree = ast.parse('1/3')
+    dump = ("Module(body=[Expr(value=BinOp(left=Call(func=Name(id='Integer', "
+            "ctx=Load()), args=[Num(n=1)], keywords=[], starargs=None, "
+            "kwargs=None), op=Div(), right=Call(func=Name(id='Integer', "
+            "ctx=Load()), args=[Num(n=3)], keywords=[], starargs=None, "
+            "kwargs=None)))])")
+    tree = IntegerWrapper().visit(tree)
+    assert ast.dump(tree) == dump
+    tree2 = ast.parse('Integer(1)/Integer(3)')
+    tree_new = IntegerWrapper().visit(tree2)
+    assert ast.dump(tree_new) == dump
+    dump3 = ("Module(body=[Expr(value=Call(func=Name(id='f', ctx=Load()), "
+             "args=[Call(func=Name(id='Integer', ctx=Load()), args=[Num(n=1)], "
+             "keywords=[], starargs=None, kwargs=None)], keywords=[], "
+             "starargs=None, kwargs=None))])")
+    tree3 = ast.parse('f(1)')
+    tree_new = IntegerWrapper().visit(tree3)
+    assert ast.dump(tree_new) == dump3
+    tree_new2 = IntegerWrapper().visit(tree_new)
+    assert ast.dump(tree_new2) == dump3
 
 
 def test_automatic_symbols():
@@ -53,7 +78,7 @@ def test_int_to_Integer():
     app.run_cell("a = 1")
     assert isinstance(app.user_ns['a'], int)
 
-    enable_automatic_int_sympification(app)
+    app.ast_transformers.append(IntegerWrapper())
     app.run_cell("a = 1/2")
     assert isinstance(app.user_ns['a'], Rational)
     app.run_cell("a = 1")
