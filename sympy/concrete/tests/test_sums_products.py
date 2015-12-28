bba137d43f8d13f0ebdf3c99378845b05e24b193
Sergey B Kirpichev <skirpichev@gmail.com>
2015-12-28 19:36:07 +0300

    Add a regression test for sympy/sympy#8016
    Mentioned in the issue problems with gamma
    series/limits - still there...

--- sympy/concrete/tests/test_sums_products.py
+++ sympy/concrete/tests/test_sums_products.py
@@ -849,3 +849,11 @@ def test_issue_4668():
 
 def test_issue_8822():
     assert summation(1/((k+3.5)*(k+8)), (k, 1, n)) is not nan
+
+
+def test_issue_8016():
+    n, m = symbols('n, m', integer=True, positive=True)
+    k = symbols('k', integer=True)
+    s = Sum(binomial(m, k)*binomial(m, n-k)*(-1)**k, (k, 0, n))
+    assert s.doit().simplify() == \
+        cos(pi*n/2)*gamma(m + 1)/gamma(n/2 + 1)/gamma(m - n/2 +1)
