8671b7025b3c92dee17bbd1301b7e936bf470323
Chris Smith <smichr@gmail.com>
2016-01-31 16:34:23 -0600

    S.Zero.as_coeff_Mul() == (1, 0) not (0, 1)
    Signed-off-by: Sergey B Kirpichev <skirpichev@gmail.com>

--- diofant/core/tests/test_expr.py
+++ diofant/core/tests/test_expr.py
@@ -1230,6 +1230,7 @@ def test_sympyissue_5300():
 
 
 def test_as_coeff_Mul():
+    assert S.Zero.as_coeff_Mul() == (S.One, S.Zero)
     assert Integer(3).as_coeff_Mul() == (Integer(3), Integer(1))
     assert Rational(3, 4).as_coeff_Mul() == (Rational(3, 4), Integer(1))
     assert Float(5.0).as_coeff_Mul() == (Float(5.0), Integer(1))
