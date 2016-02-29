diofant/diofant

commit 52af8a5848d2ab710582786f886553198094aa86

Use flake8 for code quality testing

setup.py
@@ -1,31 +1,7 @@
 #!/usr/bin/env python
 """
-Setuptools-based setup script for SymPy.
-
-This uses Setuptools (http://pythonhosted.org/setuptools/setuptools.html),
-a collection of enhancements to the standard Python distutils.
-For the easiest installation just type the command (you'll probably
-need root privileges for that):
-
-    python setup.py install
-
-This will install the library in the default location. For instructions on
-how to customize the install procedure read the output of:
-
-    python setup.py --help install
-
-In addition, there are some other commands:
-
-    python setup.py clean -> will clean all trash (*.pyc and stuff)
-    python setup.py test  -> will run the complete test suite
-    python setup.py audit -> will run pyflakes checker on source code
-
-To get a full list of avaiable commands, read the output of:
-
-    python setup.py --help-commands
-
-Or, if all else fails, feel free to write to the sympy list at
-sympy@googlegroups.com and ask for help.
+This script uses Setuptools (http://pythonhosted.org/setuptools/), a
+collection of enhancements to the standard Python distutils.
 """
 
 import sys
@@ -39,38 +15,6 @@ from setuptools.command.test import test as TestCommand
 if sys.version_info[:2] < (3, 2):
     print("SymPy requires Python 3.2 or newer. Python %d.%d detected" % sys.version_info[:2])
     sys.exit(-1)
-
-
-class audit(Command):
-    """Audits SymPy's source code for following issues:
-        - Names which are used but not defined or used before they are defined.
-        - Names which are redefined without having been used.
-    """
-
-    description = "Audit SymPy source with PyFlakes"
-    user_options = []
-
-    def initialize_options(self):
-        self.all = None
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import os
-        try:
-            import pyflakes.scripts.pyflakes as flakes
-        except ImportError:
-            print("In order to run the audit, you need to have PyFlakes installed.")
-            sys.exit(-1)
-        dirs = (os.path.join(*d) for d in (m.split('.') for m in find_packages()))
-        warns = 0
-        for dir in dirs:
-            for filename in os.listdir(dir):
-                if filename.endswith('.py') and filename != '__init__.py':
-                    warns += flakes.checkPath(os.path.join(dir, filename))
-        if warns > 0:
-            print("Audit finished with total %d warnings" % warns)
 
 
 class clean(Command):
@@ -135,13 +79,7 @@ class test(TestCommand):
 
     def run_tests(self):
         import pytest
-        import pep8
-        errno = pytest.main(self.pytest_args)
-        if errno != 0:
-            sys.exit(errno)
-        del sys.argv[:]
-        errno = pep8._main()
-        sys.exit(errno)
+        return pytest.main(self.pytest_args)
 
 
 exec(open('sympy/release.py').read())
@@ -160,8 +98,7 @@ setup(name='sympy',
       packages=find_packages(),
       ext_modules=[],
       cmdclass={'test': test,
-                'clean': clean,
-                'audit': audit},
+                'clean': clean},
       classifiers=[
           'License :: OSI Approved :: BSD License',
           'Operating System :: OS Independent',
@@ -173,7 +110,7 @@ setup(name='sympy',
           'Programming Language :: Python :: 3.2',
           'Programming Language :: Python :: 3.4',
       ],
-      tests_require=['pytest>=2.7.0', 'pep8>=1.6.0', 'pytest-cov'],
+      tests_require=['pytest>=2.7.0', 'flake8', 'pep8-naming', 'pytest-cov'],
       install_requires=['mpmath>=0.19', 'decorator', 'strategies>=0.2.3'],
       setup_requires=['setuptools>=5.5.1,<=19.4', 'pip>=6.0'],
       extras_require={

.travis.yml
@@ -29,10 +29,13 @@ matrix:
     - python: 3.4
       install:
         - travis_wait pip install .[exports,plot,interactive,gmpy]
+        # see https://gitlab.com/pycqa/flake8/issues/121
+        - pip install git+https://gitlab.com/pycqa/flake8.git@a8aac560
         - pip install sphinx numpydoc pytest-cov
       env:
         - SYMPY_GROUND_TYPES=gmpy1
       script:
+        - python setup.py flake8
         - py.test --cov sympy sympy/polys
         - sphinx-build -W -b html docs build/sphinx/html
       after_success:

setup.cfg
@@ -1,3 +1,33 @@
+[flake8]
+# E122 - continuation line missing indentation or outdented
+# E124 - closing bracket does not match visual indentation
+# E127 - continuation line over-indented for visual indent
+# E128 - continuation line under-indented for visual indent
+# E201 - whitespace after '['
+# E202 - whitespace before ']'
+# E221 - multiple spaces before operator
+# E402 - module level import not at top of file
+# E226 - missing whitespace around arithmetic operator
+# E241 - multiple spaces after ','
+# W503 - line break before binary operator
+# F401 - 'foo' imported but unused
+# F402 - import 'foo' from line bar shadowed by loop variable
+# F403 - 'from ... import *' used; unable to detect undefined names
+# F811 - redefinition of unused 'foo' from line bar
+# F821 - undefined name 'foo'
+# F823 - local variable 'foo' referenced before assignment
+# F841 - local variable 'bar' is assigned to but never used
+# N801 - class names should use CapWords convention
+# N802 - function name should be lowercase
+# N803 - argument name should be lowercase
+# N806 - variable in function should be lowercase
+# N811 - constant imported as non constant
+# N812 - lowercase imported as non lowercase
+# N813 - camelcase imported as lowercase
+# N814 - camelcase imported as constant
+ignore = E122,E124,E127,E128,E201,E202,E221,E226,E241,E402,W503,F401,F402,F403,F811,F821,F823,F841,N801,N802,N803,N806,N811,N812,N813,N814
+max-line-length = 7000
+exclude = build,.eggs
 [pep8]
 # E122 - continuation line missing indentation or outdented
 # E124 - closing bracket does not match visual indentation

commit 2fe7626c74920f1f575de1b110201781f68c6cbf

setup.py: fix test.run_tests, exit status was broken in 52af8a5

setup.py
@@ -79,7 +79,8 @@ class test(TestCommand):
 
     def run_tests(self):
         import pytest
-        return pytest.main(self.pytest_args)
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 exec(open('sympy/release.py').read())