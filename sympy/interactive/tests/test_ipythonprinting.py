dfc706ae8ceb5c3c74cc409788443fb20d0bea11
Sergey B Kirpichev <skirpichev@gmail.com>
2016-04-19 15:40:34 +0300

    Improve coverage for sympy/interactive/printing.py
    Note: this commit also drop comment for latex_formatter.for_type,
    I think after ipython/ipython#4561 this method is well documented.

--- sympy/interactive/tests/test_ipythonprinting.py
+++ sympy/interactive/tests/test_ipythonprinting.py
@@ -7,11 +7,8 @@ from sympy.external import import_module
 
 ipython = import_module("IPython", min_module_version="2.3.0")
 
-# disable tests if ipython is not present
-if not ipython:
-    disabled = True
 
-
+@pytest.mark.skipif(ipython is None, reason="no IPython")
 def test_ipythonprinting():
     # Initialize and setup IPython session
     app = init_ipython_session()
@@ -19,22 +16,24 @@ def test_ipythonprinting():
     app.run_cell("inst = ip.instance()")
     app.run_cell("format = inst.display_formatter.format")
     app.run_cell("from sympy import Symbol")
+    app.run_cell("from sympy import init_printing")
 
     # Printing by default
     app.run_cell("a = format(Symbol('pi'))")
     app.run_cell("a2 = format(Symbol('pi')**2)")
+    app.run_cell("init_printing()")
     assert app.user_ns['a'][0]['text/plain'] in ('\N{GREEK SMALL LETTER PI}', 'pi')
     assert app.user_ns['a2'][0]['text/plain'] in (' 2\n\N{GREEK SMALL LETTER PI} ', '  2\npi ')
 
     # Use different printing setup
-    app.run_cell("from sympy import init_printing")
-    app.run_cell("init_printing(use_unicode=False, pretty_print=False)")
+    app.run_cell("init_printing(use_unicode=False)")
     app.run_cell("a = format(Symbol('pi'))")
     app.run_cell("a2 = format(Symbol('pi')**2)")
     assert app.user_ns['a'][0]['text/plain'] == "pi"
-    assert app.user_ns['a2'][0]['text/plain'] == "pi**2"
+    assert app.user_ns['a2'][0]['text/plain'] == "  2\npi "
 
 
+@pytest.mark.skipif(ipython is None, reason="no IPython")
 def test_print_builtin_option():
     # Initialize and setup IPython session
     app = init_ipython_session()
@@ -42,41 +41,55 @@ def test_print_builtin_option():
     app.run_cell("inst = ip.instance()")
     app.run_cell("format = inst.display_formatter.format")
     app.run_cell("from sympy import Symbol")
-    app.run_cell("from sympy import init_printing")
+    app.run_cell("from sympy import init_printing, pretty, sstrrepr")
 
+    app.run_cell("init_printing()")
     app.run_cell("a = format({Symbol('pi'): 3.14, Symbol('n_i'): 3})")
     text = app.user_ns['a'][0]['text/plain']
     pytest.raises(KeyError, lambda: app.user_ns['a'][0]['text/latex'])
-    # Note : In Python 3 the text is unicode, but in 2 it is a string.
     # XXX: How can we make this ignore the terminal width? This test fails if
     # the terminal is too narrow.
-    assert text in ("{pi: 3.14, n_i: 3}",
-                    '{n\N{LATIN SUBSCRIPT SMALL LETTER I}: 3, \N{GREEK SMALL LETTER PI}: 3.14}',
-                    "{n_i: 3, pi: 3.14}",
+    assert text in ('{n\N{LATIN SUBSCRIPT SMALL LETTER I}: 3, \N{GREEK SMALL LETTER PI}: 3.14}',
                     '{\N{GREEK SMALL LETTER PI}: 3.14, n\N{LATIN SUBSCRIPT SMALL LETTER I}: 3}')
 
     # If we enable the default printing, then the dictionary's should render
     # as a LaTeX version of the whole dict: ${\pi: 3.14, n_i: 3}$
-    app.run_cell("inst.display_formatter.formatters['text/latex'].enabled = True")
     app.run_cell("init_printing(use_latex=True)")
+    app.run_cell("inst.display_formatter.formatters['text/latex'].enabled = True")
     app.run_cell("a = format({Symbol('pi'): 3.14, Symbol('n_i'): 3})")
     text = app.user_ns['a'][0]['text/plain']
     latex = app.user_ns['a'][0]['text/latex']
-    assert text in ("{pi: 3.14, n_i: 3}",
-                    '{n\N{LATIN SUBSCRIPT SMALL LETTER I}: 3, \N{GREEK SMALL LETTER PI}: 3.14}',
-                    "{n_i: 3, pi: 3.14}",
+    assert text in ('{n\N{LATIN SUBSCRIPT SMALL LETTER I}: 3, \N{GREEK SMALL LETTER PI}: 3.14}',
                     '{\N{GREEK SMALL LETTER PI}: 3.14, n\N{LATIN SUBSCRIPT SMALL LETTER I}: 3}')
     assert latex == r'$$\left \{ n_{i} : 3, \quad \pi : 3.14\right \}$$'
 
-    app.run_cell("inst.display_formatter.formatters['text/latex'].enabled = True")
-    app.run_cell("init_printing(use_latex=True, print_builtin=False)")
+    app.run_cell("a = format([Symbol('x'), Symbol('y')])")
+    latex = app.user_ns['a'][0]['text/latex']
+    assert latex in (r'$$\left [ x, \quad y\right ]$$',
+                     r'$$\left [ y, \quad x\right ]$$')
+
+    app.run_cell("a = format(False)")
+    assert app.user_ns['a'][0]['text/plain'] == r'False'
+
+    app.run_cell("init_printing(pretty_printer=pretty, no_global=True)")
+    app.run_cell("a = format(Symbol('pi'))")
+    assert app.user_ns['a'][0]['text/plain'] in '\N{GREEK SMALL LETTER PI}'
+
+    app.run_cell("init_printing(pretty_print=False, str_printer=sstrrepr)")
+    app.run_cell("a = format(Symbol('pi'))")
+    assert app.user_ns['a'][0]['text/plain'] == 'pi'
+
+    app.run_cell("init_printing(print_builtin=False)")
+    app.run_cell("a = format(int(1))")
+    pytest.raises(KeyError, lambda: app.user_ns['a'][0]['text/latex'])
+
+    app.run_cell("init_printing(ip=ip, use_latex=False)")
     app.run_cell("a = format({Symbol('pi'): 3.14, Symbol('n_i'): 3})")
     text = app.user_ns['a'][0]['text/plain']
     pytest.raises(KeyError, lambda: app.user_ns['a'][0]['text/latex'])
-    # Note : In Python 3 the text is unicode, but in 2 it is a string.
-    assert text in ("{pi: 3.14, n_i: 3}", "{n_i: 3, pi: 3.14}")
 
 
+@pytest.mark.skipif(ipython is None, reason="no IPython")
 def test_matplotlib_bad_latex():
     # Initialize and setup IPython session
     app = init_ipython_session()
@@ -85,10 +98,7 @@ def test_matplotlib_bad_latex():
     app.run_cell("inst = ip.instance()")
     app.run_cell("format = inst.display_formatter.format")
     app.run_cell("from sympy import init_printing, Matrix")
-    app.run_cell("init_printing(use_latex='matplotlib')")
-
-    # The png formatter is not enabled by default in this context
-    app.run_cell("inst.display_formatter.formatters['image/png'].enabled = True")
+    app.run_cell("init_printing()")
 
     # Make sure no warnings are raised by IPython
     app.run_cell("import warnings")
