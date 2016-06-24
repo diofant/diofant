da9789c6cd7d0c2ceeea19fbf59645987125b289
Sergey B Kirpichev <skirpichev@gmail.com>
2016-06-24 23:42:00 +0300

    Build correct inhomogeneous solution in rsolve_hyper
    Use rsolve_ratio instead.  (If it returns a generic
    solution - we substitute back 0's for symbols in one.)
    
    Fixes diofant/diofant#294
    
    Closes sympy/sympy#11261
    Closes sympy/sympy#7055

--- sympy/solvers/recurr.py
+++ sympy/solvers/recurr.py
@@ -558,14 +558,19 @@ def rsolve_hyper(coeffs, f, n, **hints):
             for j in range(0, r + 1):
                 polys[j] *= Mul(*(denoms[:j] + denoms[j + 1:]))
 
-            R = rsolve_poly(polys, Mul(*denoms), n)
+            R = rsolve_ratio(polys, Mul(*denoms), n, symbols=True)
+            if R is not None:
+                R, syms = R
+                if syms:
+                    R = R.subs(zip(syms, [0]*len(syms)))
 
-            if not (R is None or R is S.Zero):
+            if R:
                 inhomogeneous[i] *= R
             else:
                 return
 
             result = Add(*inhomogeneous)
+            result = simplify(result)
     else:
         result = S.Zero
 
--- sympy/solvers/tests/test_recurr.py
+++ sympy/solvers/tests/test_recurr.py
@@ -95,9 +95,9 @@ def test_rsolve_bulk():
             q = recurrence_term(c, p)
             if p.is_polynomial(n):
                 assert rsolve_poly(c, q, n) == p
-            # See issue sympy/sympy#7055:
-            # if p.is_hypergeometric(n):
-            #    assert rsolve_hyper(c, q, n) == p
+            if p.is_hypergeometric(n) and len(c) <= 3:
+                assert rsolve_hyper(c, q, n).subs(zip(symbols('C:3'),
+                                                      [0, 0, 0])).expand() == p
 
 
 @pytest.mark.slow
@@ -207,3 +207,13 @@ def test_issue_6844():
     f = y(n + 2) - y(n + 1) + y(n)/4
     assert rsolve(f, y(n)) == 2**(-n)*(C0 + C1*n)
     assert rsolve(f, y(n), {y(0): 0, y(1): 1}) == 2*2**(-n)*n
+
+
+def test_diofantissue_294():
+    f = y(n) - y(n - 1) - 2*y(n - 2) - 2*n
+    assert rsolve(f, y(n)) == (-1)**n*C0 + 2**n*C1 - n - Rational(5, 2)
+    # issue sympy/sympy#11261
+    assert rsolve(f, y(n), {y(0): -1, y(1): 1}) == (-(-1)**n/2 + 2*2**n -
+                                                    n - Rational(5, 2))
+    # issue sympy/sympy#7055
+    assert rsolve(-2*y(n) + y(n + 1) + n - 1, y(n)) == 2**n*C0 + n
