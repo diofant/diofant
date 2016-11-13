8ba74a5945306877243c99826fb222328be98f11
Sergey B Kirpichev <skirpichev@gmail.com>
2016-11-13 18:35:38 +0300

    Split travis jobs.  It seems, now timeout again is 50min
    This mostly taken from 523cae9.

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import warnings
 
@@ -6,12 +7,38 @@ import pytest
 from diofant.core.cache import clear_cache, USE_CACHE
 from diofant.core.compatibility import GROUND_TYPES
 
+sp = re.compile(r'([0-9]+)/([1-9][0-9]*)')
+
+
+def process_split(session, config, items):
+    split = config.getoption("--split")
+    if not split:
+        return
+    m = sp.match(split)
+    if not m:
+        raise ValueError("split must be a string of the form a/b "
+                         "where a and b are ints.")
+    i, t = map(int, m.groups())
+    start, end = (i - 1)*len(items)//t, i*len(items)//t
+
+    if i < t:
+        del items[end:]
+    del items[:start]
+
 
 def pytest_report_header(config):
     return """
 cache: %s
 ground types: %s
 """ % (USE_CACHE, GROUND_TYPES)
+
+
+def pytest_addoption(parser):
+    parser.addoption("--split", action="store", default="", help="split tests")
+
+
+def pytest_collection_modifyitems(session, config, items):
+    process_split(session, config, items)
 
 
 @pytest.fixture(autouse=True, scope='module')
