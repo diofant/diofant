60bd33fd9139a472fb80f6bc722db8755d8928be
Sergey B Kirpichev <skirpichev@gmail.com>
2016-11-22 13:04:17 +0300

    Drop override theme.css
    See snide/sphinx_rtd_theme#329

--- docs/conf.py
+++ docs/conf.py
@@ -134,24 +134,9 @@ intersphinx_mapping = {
     'numpy': ('https://docs.scipy.org/doc/numpy', None),
 }
 
-html_static_path = ['_static']
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',
-    ],
-}
-
 # RTD theme support:
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-else:
-    html_context = {
-        'css_files': [
-            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-            '_static/theme_overrides.css',
-        ],
-    }
