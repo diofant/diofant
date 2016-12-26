087ee7dd31430dfcb919504b5f843f359b76912a
Sergey B Kirpichev <skirpichev@gmail.com>
2016-12-26 14:11:22 +0300

    Add tests for fourier.py

--- diofant/matrices/expressions/tests/test_fourier.py
+++ diofant/matrices/expressions/tests/test_fourier.py
@@ -1,4 +1,4 @@
-from diofant import S, I, Abs, simplify, exp, sqrt
+from diofant import pi, I, Abs, simplify, exp, sqrt
 from diofant.matrices.expressions.fourier import DFT, IDFT
 from diofant.matrices import det, Matrix, Identity
 from diofant.abc import n, i, j
@@ -8,4 +8,11 @@ def test_dft():
     assert DFT(4).shape == (4, 4)
     assert Abs(simplify(det(Matrix(DFT(4))))) == 1
     assert DFT(n)*IDFT(n) == Identity(n)
-    assert DFT(n)[i, j] == exp(-2*S.Pi*I/n)**(i*j) / sqrt(n)
+    assert DFT(n)[i, j] == exp(-2*pi*I/n)**(i*j) / sqrt(n)
+    assert DFT(n).inverse() == IDFT(n)
+
+
+def test_idft():
+    assert IDFT(3).shape == (3, 3)
+    assert IDFT(n)[i, j] == (exp(-2*I*pi/n))**(-i*j)/sqrt(n)
+    assert IDFT(n).inverse() == DFT(n)
