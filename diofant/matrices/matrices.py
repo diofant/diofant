73772322ad66f48e5009d787c9a3c56d06f49454
Sergey B Kirpichev <skirpichev@gmail.com>
2017-02-01 23:05:03 +0300

    Drop unused _jordan_split helper method

--- diofant/matrices/matrices.py
+++ diofant/matrices/matrices.py
@@ -3738,16 +3738,6 @@ class MatrixBase(DefaultPrinting):
 
         return type(self)(P), Jcells
 
-    def _jordan_split(self, algebraical, geometrical):
-        """Return a list of integers with sum equal to 'algebraical'
-        and length equal to 'geometrical'
-        """
-        n1 = algebraical // geometrical
-        res = [n1]*geometrical
-        res[len(res) - 1] += algebraical % geometrical
-        assert sum(res) == algebraical
-        return res
-
     def has(self, *patterns):
         """Test whether any subexpression matches any of the patterns.
 
