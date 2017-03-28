556b879b58f7866177f7897070d4052cc533c5f5
Sergey B Kirpichev <skirpichev@gmail.com>
2017-03-28 08:52:27 +0300

    100% test coverage for matadd.py

--- diofant/matrices/expressions/tests/test_adjoint.py
+++ diofant/matrices/expressions/tests/test_adjoint.py
@@ -9,6 +9,7 @@ n, m, l, k, p = symbols('n m l k p', integer=True)
 A = MatrixSymbol('A', n, m)
 B = MatrixSymbol('B', m, l)
 C = MatrixSymbol('C', n, n)
+D = MatrixSymbol('D', n, n)
 
 
 def test_adjoint():
@@ -34,3 +35,4 @@ def test_adjoint():
     assert Adjoint(Sq)[0, 1] == conjugate(Sq[1, 0])
 
     assert Adjoint(A*B).doit() == Adjoint(B) * Adjoint(A)
+    assert Adjoint(C + D).doit() == Adjoint(C) + Adjoint(D)
