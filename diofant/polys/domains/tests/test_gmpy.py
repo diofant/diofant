810052fee9f299be364c0d17a86bdd13bb7e281a
Sergey B Kirpichev <skirpichev@gmail.com>
2017-03-28 17:54:35 +0300

    100% test coverage for gmpyintegerring.py

--- diofant/polys/domains/tests/test_gmpy.py
+++ diofant/polys/domains/tests/test_gmpy.py
@@ -1,7 +1,8 @@
 import pytest
 
 from diofant.polys.polyerrors import CoercionFailed
-from diofant.polys.domains import FF, QQ_python, QQ_gmpy, PythonRational
+from diofant.polys.domains import (FF, QQ_python, QQ_gmpy, ZZ_gmpy,
+                                   PythonRational)
 from diofant.external import import_module
 
 __all__ = ()
@@ -16,6 +17,12 @@ def test_convert():
     assert F3.convert(gmpy.mpz(2)) == F3.dtype(2)
     assert F3.convert(gmpy.mpq(2, 1)) == F3.dtype(2)
     pytest.raises(CoercionFailed, lambda: F3.convert(gmpy.mpq(1, 2)))
+
+    assert ZZ_gmpy().convert(F3(1)) == ZZ_gmpy().dtype(1)
+
+    assert ZZ_gmpy().convert(PythonRational(2)) == ZZ_gmpy().dtype(2)
+    pytest.raises(CoercionFailed,
+                  lambda: ZZ_gmpy().convert(PythonRational(2, 3)))
 
     assert QQ_python().convert(gmpy.mpz(3)) == QQ_python().dtype(3)
     assert QQ_python().convert(gmpy.mpq(2, 3)) == QQ_python().dtype(2, 3)
