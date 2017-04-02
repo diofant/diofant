c014f2b276a800f249accb2302389f529cff21c0
Sergey B Kirpichev <skirpichev@gmail.com>
2017-04-02 18:50:11 +0300

    100% test coverage for matexpr.py

--- diofant/matrices/expressions/__init__.py
+++ diofant/matrices/expressions/__init__.py
@@ -5,8 +5,7 @@ from .blockmatrix import BlockMatrix, BlockDiagMatrix, block_collapse, blockcut
 from .funcmatrix import FunctionMatrix
 from .inverse import Inverse
 from .matadd import MatAdd
-from .matexpr import (Identity, MatrixExpr, MatrixSymbol, ZeroMatrix,
-                      matrix_symbols)
+from .matexpr import Identity, MatrixExpr, MatrixSymbol, ZeroMatrix
 from .matmul import MatMul
 from .matpow import MatPow
 from .trace import Trace, trace
