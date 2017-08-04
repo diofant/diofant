562a58c870746373c0d7a4e80f2630e325c8be68
Sergey B Kirpichev <skirpichev@gmail.com>
2017-08-04 21:38:49 +0300

    Fix IntegerDivisionWrapper for negative rationals

--- diofant/interactive/tests/test_ipython.py
+++ diofant/interactive/tests/test_ipython.py
@@ -44,3 +44,9 @@ def test_IntegerDivisionWrapper():
     tree = ast.parse('1 + 3')
     tree_new = IntegerDivisionWrapper().visit(tree)
     assert ast.dump(tree_new) == ast.dump(tree)
+
+    tree = ast.parse('-1/3')
+    tree2 = ast.parse('Rational(-1, 3)')
+    dump = ast.dump(tree2)
+    tree_new = IntegerDivisionWrapper().visit(tree)
+    assert ast.dump(tree_new) == dump
