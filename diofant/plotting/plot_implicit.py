diofant/diofant

commit 7ac5c96b8609d61c6cc7d67df46360356e5a83a6

This removes experimental_lambdify() and intervalmath submodule
There are regressions with implicit plotting and
some complex expressions (e.g. Sum's and Integral's,
which are slow anyway).

Closes sympy/sympy#11461
Closes sympy/sympy#10925
Closes diofant/diofant#336

diofant/plotting/plot_implicit.py
@@ -1,33 +1,17 @@
 """Implicit plotting module for Diofant
 
 The module implements a data series called ImplicitSeries which is used by
-``Plot`` class to plot implicit plots for different backends. The module,
-by default, implements plotting using interval arithmetic. It switches to a
-fall back algorithm if the expression cannot be plotted using interval arithmetic.
-It is also possible to specify to use the fall back algorithm for all plots.
-
-Boolean combinations of expressions cannot be plotted by the fall back
-algorithm.
+``Plot`` class to plot implicit plots for different backends.
 
 See Also
 ========
+
 diofant.plotting.plot
-
-References
-==========
-- Jeffrey Allen Tupper. Reliable Two-Dimensional Graphing Methods for
-Mathematical Formulae with Two Free Variables.
-
-- Jeffrey Allen Tupper. Graphing Equations with Generalized Interval
-Arithmetic. Master's thesis. University of Toronto, 1996
-
 """
 
 import warnings
 
 from .plot import BaseSeries, Plot
-from .experimental_lambdify import experimental_lambdify, vectorized_lambdify
-from .intervalmath import interval
 from ..core.relational import (Equality, GreaterThan, LessThan,
                                Relational, StrictLessThan, StrictGreaterThan)
 from ..core import Eq, Tuple, sympify, Symbol, Dummy
@@ -35,7 +19,11 @@ from ..external import import_module
 from ..logic.boolalg import BooleanFunction
 from ..polys.polyutils import _sort_gens
 from ..utilities.decorator import doctest_depends_on
-from ..utilities import flatten
+from ..utilities import flatten, lambdify
+
+
+def vectorized_lambdify(args, expr):
+    return lambdify(args, expr, "numpy")
 
 
 class ImplicitSeries(BaseSeries):
@@ -64,109 +52,8 @@ class ImplicitSeries(BaseSeries):
         self.depth = 4 + depth
         self.line_color = line_color
 
-    def __str__(self):
-        return ('Implicit equation: %s for '
-                '%s over %s and %s over %s') % (
-                    str(self.expr),
-                    str(self.var_x),
-                    str((self.start_x, self.end_x)),
-                    str(self.var_y),
-                    str((self.start_y, self.end_y)))
-
     def get_raster(self):
-        func = experimental_lambdify((self.var_x, self.var_y), self.expr,
-                                    use_interval=True)
-        xinterval = interval(self.start_x, self.end_x)
-        yinterval = interval(self.start_y, self.end_y)
-        try:
-            temp = func(xinterval, yinterval)
-        except (AttributeError, NameError):
-            if self.use_interval_math:
-                warnings.warn("Adaptive meshing could not be applied to the"
-                            " expression. Using uniform meshing.")
-            self.use_interval_math = False
-
-        if self.use_interval_math:
-            return self._get_raster_interval(func)
-        else:
-            return self._get_meshes_grid()
-
-    def _get_raster_interval(self, func):
-        """ Uses interval math to adaptively mesh and obtain the plot"""
-        k = self.depth
-        interval_list = []
-        # Create initial 32 divisions
-        np = import_module('numpy')
-        xsample = np.linspace(self.start_x, self.end_x, 33)
-        ysample = np.linspace(self.start_y, self.end_y, 33)
-
-        # Add a small jitter so that there are no false positives for equality.
-        # Ex: y==x becomes True for x interval(1, 2) and y interval(1, 2)
-        # which will draw a rectangle.
-        jitterx = (np.random.rand(
-            len(xsample)) * 2 - 1) * (self.end_x - self.start_x) / 2**20
-        jittery = (np.random.rand(
-            len(ysample)) * 2 - 1) * (self.end_y - self.start_y) / 2**20
-        xsample += jitterx
-        ysample += jittery
-
-        xinter = [interval(x1, x2) for x1, x2 in zip(xsample[:-1],
-                           xsample[1:])]
-        yinter = [interval(y1, y2) for y1, y2 in zip(ysample[:-1],
-                           ysample[1:])]
-        interval_list = [[x, y] for x in xinter for y in yinter]
-        plot_list = []
-
-        # recursive call refinepixels which subdivides the intervals which are
-        # neither True nor False according to the expression.
-        def refine_pixels(interval_list):
-            """ Evaluates the intervals and subdivides the interval if the
-            expression is partially satisfied.
-            """
-            temp_interval_list = []
-            plot_list = []
-            for intervals in interval_list:
-
-                # Convert the array indices to x and y values
-                intervalx = intervals[0]
-                intervaly = intervals[1]
-                func_eval = func(intervalx, intervaly)
-                # The expression is valid in the interval. Change the contour
-                # array values to 1.
-                if func_eval[1] is False or func_eval[0] is False:
-                    pass
-                elif func_eval == (True, True):
-                    plot_list.append([intervalx, intervaly])
-                elif func_eval[1] is None or func_eval[0] is None:
-                    # Subdivide
-                    avgx = intervalx.mid
-                    avgy = intervaly.mid
-                    a = interval(intervalx.start, avgx)
-                    b = interval(avgx, intervalx.end)
-                    c = interval(intervaly.start, avgy)
-                    d = interval(avgy, intervaly.end)
-                    temp_interval_list.append([a, c])
-                    temp_interval_list.append([a, d])
-                    temp_interval_list.append([b, c])
-                    temp_interval_list.append([b, d])
-            return temp_interval_list, plot_list
-
-        while k >= 0 and len(interval_list):
-            interval_list, plot_list_temp = refine_pixels(interval_list)
-            plot_list.extend(plot_list_temp)
-            k = k - 1
-        # Check whether the expression represents an equality
-        # If it represents an equality, then none of the intervals
-        # would have satisfied the expression due to floating point
-        # differences. Add all the undecided values to the plot.
-        if self.has_equality:
-            for intervals in interval_list:
-                intervalx = intervals[0]
-                intervaly = intervals[1]
-                func_eval = func(intervalx, intervaly)
-                if func_eval[1] and func_eval[0] is not False:
-                    plot_list.append([intervalx, intervaly])
-        return plot_list, 'fill'
+        return self._get_meshes_grid()
 
     def _get_meshes_grid(self):
         """Generates the mesh for generating a contour.
@@ -293,17 +180,6 @@ def plot_implicit(expr, x_var=None, y_var=None, **kwargs):
     Plotting regions.
 
     >>> p6 = plot_implicit(y > x**2)
-
-    Plotting Using boolean conjunctions.
-
-    >>> p7 = plot_implicit(And(y > x, y > -x))
-
-    When plotting an expression with a single variable (y - 1, for example),
-    specify the x or the y variable explicitly:
-
-    >>> p8 = plot_implicit(y - 1, y_var=y)
-    >>> p9 = plot_implicit(x - 1, x_var=x)
-
     """
 
     # Represents whether the expression contains an Equality,
@@ -364,7 +240,7 @@ def plot_implicit(expr, x_var=None, y_var=None, **kwargs):
             xyvar.append(undeclared.pop())
     var_start_end_y = _range_tuple(xyvar[1])
 
-    use_interval = kwargs.pop('adaptive', True)
+    use_interval = kwargs.pop('adaptive', False)
     nb_of_points = kwargs.pop('points', 300)
     depth = kwargs.pop('depth', 0)
     line_color = kwargs.pop('line_color', "blue")

diofant/utilities/lambdify.py
@@ -101,7 +101,7 @@ MODULES = {
     "diofant": (DIOFANT, DIOFANT_DEFAULT, {}, (
         "from diofant.functions import *",
         "from diofant.matrices import *",
-        "from diofant import Integral, pi, oo, nan, zoo, E, I",)),
+        "from diofant import Sum, Integral, pi, oo, nan, zoo, E, I",)),
     "numexpr": (NUMEXPR, NUMEXPR_DEFAULT, NUMEXPR_TRANSLATIONS,
                 ("import_module('numexpr')", )),
 }

diofant/plotting/intervalmath/__init__.py
@@ -1,8 +1,0 @@
-"""
-Interval arithmetics for plotting package.
-"""
-
-from .interval_arithmetic import interval
-from .lib_interval import (Abs, exp, log, log10, atan, sin, cos, tan, sqrt,
-                          imin, imax, sinh, cosh, tanh, acosh, asinh, atanh,
-                          asin, acos, ceil, floor, And, Or)

diofant/plotting/intervalmath/interval_arithmetic.py
@@ -1,449 +1,0 @@
-"""
-Interval Arithmetic for plotting.
-This module does not implement interval arithmetic accurately and
-hence cannot be used for purposes other than plotting. If you want
-to use interval arithmetic, use mpmath's interval arithmetic.
-
-The module implements interval arithmetic using numpy and
-python floating points. The rounding up and down is not handled
-and hence this is not an accurate implementation of interval
-arithmetic.
-
-The module uses numpy for speed which cannot be achieved with mpmath.
-"""
-
-# Q: Why use numpy? Why not simply use mpmath's interval arithmetic?
-# A: mpmath's interval arithmetic simulates a floating point unit
-# and hence is slow, while numpy evaluations are orders of magnitude
-# faster.
-
-# Q: Why create a separate class for intervals? Why not use diofant's
-# Interval Sets?
-# A: The functionalities that will be required for plotting is quite
-# different from what Interval Sets implement.
-
-# Q: Why is rounding up and down according to IEEE754 not handled?
-# A: It is not possible to do it in both numpy and python. An external
-# library has to used, which defeats the whole purpose i.e., speed. Also
-# rounding is handled for very few functions in those libraries.
-
-# Q Will my plots be affected?
-# A It will not affect most of the plots. The interval arithmetic
-# module based suffers the same problems as that of floating point
-# arithmetic.
-
-from ...simplify import nsimplify
-
-
-class interval:
-    """ Represents an interval containing floating points as start and
-    end of the interval
-    The is_valid variable tracks whether the interval obtained as the
-    result of the function is in the domain and is continuous.
-    - True: Represents the interval result of a function is continuous and
-            in the domain of the function.
-    - False: The interval argument of the function was not in the domain of
-             the function, hence the is_valid of the result interval is False
-    - None: The function was not continuous over the interval or
-            the function's argument interval is partly in the domain of the
-            function
-
-    The comparison of two intervals returns a tuple of two 3-valued logic
-    values.
-
-    The first value determines the comparison as follows:
-    - True: If the comparison is True throughout the intervals.
-    - False: If the comparison is False throughout the intervals.
-    - None: If the comparison is True for some part of the intervals.
-
-    The second value is determined as follows:
-    - True: If both the intervals in comparison are valid.
-    - False: If at least one of the intervals is False, else
-    - None
-    """
-
-    def __init__(self, *args, **kwargs):
-        self.is_valid = kwargs.pop('is_valid', True)
-        if len(args) == 1:
-            if isinstance(args[0], interval):
-                self.start, self.end = args[0].start, args[0].end
-            else:
-                self.start = float(args[0])
-                self.end = float(args[0])
-        elif len(args) == 2:
-            if args[0] < args[1]:
-                self.start = float(args[0])
-                self.end = float(args[1])
-            else:
-                self.start = float(args[1])
-                self.end = float(args[0])
-
-        else:
-            raise ValueError("interval takes a maximum of two float values "
-                            "as arguments")
-
-    @property
-    def mid(self):
-        return (self.start + self.end) / 2.0
-
-    @property
-    def width(self):
-        return self.end - self.start
-
-    def __repr__(self):
-        return "interval(%f, %f)" % (self.start, self.end)
-
-    def __str__(self):
-        return "[%f, %f]" % (self.start, self.end)
-
-    def __lt__(self, other):
-        if isinstance(other, (int, float)):
-            if self.end < other:
-                return True, self.is_valid
-            elif self.start > other:
-                return False, self.is_valid
-            else:
-                return None, self.is_valid
-
-        elif isinstance(other, interval):
-            if self.is_valid is False or other.is_valid is False:
-                valid = False
-            elif self.is_valid is None or other.is_valid is None:
-                valid = None
-            else:
-                valid = True
-            if self.end < other. start:
-                return True, valid
-            if self.start > other.end:
-                return False, valid
-            return None, valid
-        else:
-            return NotImplemented
-
-    def __gt__(self, other):
-        if isinstance(other, (int, float)):
-            if self.start > other:
-                return True, self.is_valid
-            elif self.end < other:
-                return False, self.is_valid
-            else:
-                return None, self.is_valid
-        elif isinstance(other, interval):
-            return other.__lt__(self)
-        else:
-            return NotImplemented
-
-    def __eq__(self, other):
-        if isinstance(other, (int, float)):
-            if self.start == other and self.end == other:
-                return True, self.is_valid
-            if other in self:
-                return None, self.is_valid
-            else:
-                return False, self.is_valid
-
-        if isinstance(other, interval):
-            if self.is_valid is False or other.is_valid is False:
-                valid = False
-            elif self.is_valid is None or other.is_valid is None:
-                valid = None
-            else:
-                valid = True
-            if self.start == other.start and self.end == other.end:
-                return True, valid
-            elif self.__lt__(other)[0] is not None:
-                return False, valid
-            else:
-                return None, valid
-        else:
-            return NotImplemented
-
-    def __ne__(self, other):
-        if isinstance(other, (int, float)):
-            if self.start == other and self.end == other:
-                return False, self.is_valid
-            if other in self:
-                return None, self.is_valid
-            else:
-                return True, self.is_valid
-
-        if isinstance(other, interval):
-            if self.is_valid is False or other.is_valid is False:
-                valid = False
-            elif self.is_valid is None or other.is_valid is None:
-                valid = None
-            else:
-                valid = True
-            if self.start == other.start and self.end == other.end:
-                return False, valid
-            if not self.__lt__(other)[0] is None:
-                return True, valid
-            return None, valid
-        else:
-            return NotImplemented
-
-    def __le__(self, other):
-        if isinstance(other, (int, float)):
-            if self.end <= other:
-                return True, self.is_valid
-            if self.start > other:
-                return False, self.is_valid
-            else:
-                return None, self.is_valid
-
-        if isinstance(other, interval):
-            if self.is_valid is False or other.is_valid is False:
-                valid = False
-            elif self.is_valid is None or other.is_valid is None:
-                valid = None
-            else:
-                valid = True
-            if self.end <= other.start:
-                return True, valid
-            if self.start > other.end:
-                return False, valid
-            return None, valid
-        else:
-            return NotImplemented
-
-    def __ge__(self, other):
-        if isinstance(other, (int, float)):
-            if self.start >= other:
-                return True, self.is_valid
-            elif self.end < other:
-                return False, self.is_valid
-            else:
-                return None, self.is_valid
-        elif isinstance(other, interval):
-            return other.__le__(self)
-
-    def __add__(self, other):
-        if isinstance(other, (int, float)):
-            if self.is_valid:
-                return interval(self.start + other, self.end + other)
-            else:
-                start = self.start + other
-                end = self.end + other
-                return interval(start, end, is_valid=self.is_valid)
-
-        elif isinstance(other, interval):
-            start = self.start + other.start
-            end = self.end + other.end
-            if self.is_valid and other.is_valid:
-                return interval(start, end)
-            elif self.is_valid is False or other.is_valid is False:
-                return interval(start, end, is_valid=False)
-            else:
-                return interval(start, end, is_valid=None)
-        else:
-            return NotImplemented
-
-    __radd__ = __add__
-
-    def __sub__(self, other):
-        if isinstance(other, (int, float)):
-            start = self.start - other
-            end = self.end - other
-            return interval(start, end, is_valid=self.is_valid)
-
-        elif isinstance(other, interval):
-            start = self.start - other.end
-            end = self.end - other.start
-            if self.is_valid and other.is_valid:
-                return interval(self.start - other.end, self.end - other.start)
-            elif self.is_valid is False or other.is_valid is False:
-                return interval(start, end, is_valid=False)
-            else:
-                return interval(start, end, is_valid=None)
-        else:
-            return NotImplemented
-
-    def __rsub__(self, other):
-        if isinstance(other, (int, float)):
-            start = other - self.end
-            end = other - self.start
-            return interval(start, end, is_valid=self.is_valid)
-        elif isinstance(other, interval):
-            return other.__sub__(self)
-        else:
-            return NotImplemented
-
-    def __neg__(self):
-        if self.is_valid:
-            return interval(-self.end, -self.start)
-        else:
-            return interval(-self.end, -self.start, is_valid=self.is_valid)
-
-    def __mul__(self, other):
-        if isinstance(other, interval):
-            if self.is_valid is False or other.is_valid is False:
-                return interval(-float('inf'), float('inf'), is_valid=False)
-            elif self.is_valid is None or other.is_valid is None:
-                return interval(-float('inf'), float('inf'), is_valid=None)
-            else:
-                inters = []
-                inters.append(self.start * other.start)
-                inters.append(self.end * other.start)
-                inters.append(self.start * other.end)
-                inters.append(self.end * other.end)
-                start = min(inters)
-                end = max(inters)
-                return interval(start, end)
-        elif isinstance(other, (int, float)):
-            return interval(self.start*other, self.end*other, is_valid=self.is_valid)
-        else:
-            return NotImplemented
-
-    __rmul__ = __mul__
-
-    def __contains__(self, other):
-        if isinstance(other, (int, float)):
-            return self.start <= other and self.end >= other
-        else:
-            return self.start <= other.start and other.end <= self.end
-
-    def __rdiv__(self, other):
-        if isinstance(other, (int, float)):
-            other = interval(other)
-            return other.__div__(self)
-        elif isinstance(other, interval):
-            return other.__div__(self)
-        else:
-            return NotImplemented
-
-    def __div__(self, other):
-        # Both None and False are handled
-        if not self.is_valid:
-            # Don't divide as the value is not valid
-            return interval(-float('inf'), float('inf'), is_valid=self.is_valid)
-        if isinstance(other, (int, float)):
-            if other == 0:
-                # Divide by zero encountered. valid nowhere
-                return interval(-float('inf'), float('inf'), is_valid=False)
-            else:
-                return interval(self.start / other, self.end / other)
-
-        elif isinstance(other, interval):
-            if other.is_valid is False or self.is_valid is False:
-                return interval(-float('inf'), float('inf'), is_valid=False)
-            elif other.is_valid is None or self.is_valid is None:
-                return interval(-float('inf'), float('inf'), is_valid=None)
-            else:
-                # denominator contains both signs, i.e. being divided by zero
-                # return the whole real line with is_valid = None
-                if 0 in other:
-                    return interval(-float('inf'), float('inf'), is_valid=None)
-
-                # denominator negative
-                this = self
-                if other.end < 0:
-                    this = -this
-                    other = -other
-
-                # denominator positive
-                inters = []
-                inters.append(this.start / other.start)
-                inters.append(this.end / other.start)
-                inters.append(this.start / other.end)
-                inters.append(this.end / other.end)
-                start = max(inters)
-                end = min(inters)
-                return interval(start, end)
-        else:
-            return NotImplemented
-
-    __truediv__ = __div__
-    __rtruediv__ = __rdiv__
-
-    def __pow__(self, other):
-        # Implements only power to an integer.
-        from .lib_interval import exp, log
-        if not self.is_valid:
-            return self
-        if isinstance(other, interval):
-            return exp(other * log(self))
-        elif isinstance(other, (float, int)):
-            if other < 0:
-                return 1 / self.__pow__(abs(other))
-            else:
-                if int(other) == other:
-                    return _pow_int(self, other)
-                else:
-                    return _pow_float(self, other)
-        else:
-            return NotImplemented
-
-    def __rpow__(self, other):
-        if isinstance(other, (float, int)):
-            if not self.is_valid:
-                # Don't do anything
-                return self
-            elif other < 0:
-                if self.width > 0:
-                    return interval(-float('inf'), float('inf'), is_valid=False)
-                else:
-                    power_rational = nsimplify(self.start)
-                    num, denom = power_rational.as_numer_denom()
-                    if denom % 2 == 0:
-                        return interval(-float('inf'), float('inf'),
-                                        is_valid=False)
-                    else:
-                        start = -abs(other)**self.start
-                        end = start
-                        return interval(start, end)
-            else:
-                return interval(other**self.start, other**self.end)
-        elif isinstance(other, interval):
-            return other.__pow__(self)
-        else:
-            return NotImplemented
-
-    def __hash__(self):
-        return hash((self.is_valid, self.start, self.end))
-
-
-def _pow_float(inter, power):
-    """Evaluates an interval raised to a floating point."""
-    power_rational = nsimplify(power)
-    num, denom = power_rational.as_numer_denom()
-    if num % 2 == 0:
-        start = abs(inter.start)**power
-        end = abs(inter.end)**power
-        if start < 0:
-            ret = interval(0, max(start, end))
-        else:
-            ret = interval(start, end)
-        return ret
-    elif denom % 2 == 0:
-        if inter.end < 0:
-            return interval(-float('inf'), float('inf'), is_valid=False)
-        elif inter.start < 0:
-            return interval(0, inter.end**power, is_valid=None)
-        else:
-            return interval(inter.start**power, inter.end**power)
-    else:
-        if inter.start < 0:
-            start = -abs(inter.start)**power
-        else:
-            start = inter.start**power
-
-        if inter.end < 0:
-            end = -abs(inter.end)**power
-        else:
-            end = inter.end**power
-
-        return interval(start, end, is_valid=inter.is_valid)
-
-
-def _pow_int(inter, power):
-    """Evaluates an interval raised to an integer power"""
-    power = int(power)
-    if power & 1:
-        return interval(inter.start**power, inter.end**power)
-    else:
-        if inter.start < 0 and inter.end > 0:
-            start = 0
-            end = max(inter.start**power, inter.end**power)
-            return interval(start, end)
-        else:
-            return interval(inter.start**power, inter.end**power)

commit f520ef8738d51b4b541d8a4f71799c55a06e15d3

Naive filtering of "wrong" characters in LambdaPrinter._print_Dummy
This fixes some doctests, broken by 7ac5c96b8.

Also use lambdarepr in lambdastr for fallback.

diofant/plotting/plot_implicit.py
@@ -178,6 +178,12 @@ def plot_implicit(expr, x_var=None, y_var=None, **kwargs):
     Plotting regions.
 
     >>> p6 = plot_implicit(y > x**2)
+
+    When plotting an expression with a single variable (y - 1, for example),
+    specify the x or the y variable explicitly:
+
+    >>> p8 = plot_implicit(y - 1, y_var=y)
+    >>> p9 = plot_implicit(x - 1, x_var=x)
     """
 
     # Represents whether the expression contains an Equality,

diofant/printing/lambdarepr.py
@@ -77,6 +77,9 @@ class LambdaPrinter(StrPrinter):
         ]
         return ''.join(result)
 
+    def _print_Dummy(self, expr):
+        return super()._print_Dummy(expr).replace("(", "_lpar_").replace(")", "_rpar_")
+
 
 class NumPyPrinter(LambdaPrinter):
     """

diofant/utilities/lambdify.py
@@ -457,9 +457,9 @@ def lambdastr(args, expr, printer=None, dummify=False):
             if isinstance(args, (Function, Symbol)):
                 dummies = Dummy()
                 dummies_dict.update({args: dummies})
-                return str(dummies)
+                return lambdarepr(dummies)
             else:
-                return str(args)
+                return lambdarepr(args)
 
     def sub_expr(expr, dummies_dict):
         try:
@@ -483,7 +483,7 @@ def lambdastr(args, expr, printer=None, dummify=False):
 
     if isiter(args) and any(isiter(i) for i in args):
         import re
-        dum_args = [str(Dummy(str(i))) for i in range(len(args))]
+        dum_args = [lambdarepr(Dummy(str(i))) for i in range(len(args))]
         iter_args = ','.join([i if isiter(a) else i
                               for i, a in zip(dum_args, args)])
         lstr = lambdastr(flatten(args), expr, printer=printer, dummify=dummify)

diofant/printing/tests/test_lambdarepr.py
@@ -1,6 +1,7 @@
 import pytest
 
-from diofant import And, Interval, Matrix, Not, Or, Piecewise, false, sin
+from diofant import (And, Dummy, Interval, Matrix, Not, Or, Piecewise, false,
+                     sin)
 from diofant.abc import x, y, z
 from diofant.printing.lambdarepr import lambdarepr
 
@@ -16,6 +17,7 @@ def test_basic():
     assert lambdarepr(Or(x, y)) == "((x) or (y))"
     assert lambdarepr(Not(x)) == "(not (x))"
     assert lambdarepr(false) == "False"
+    assert lambdarepr(Dummy('f(x)')) == "_f_lpar_x_rpar_"
 
 
 def test_matrix():