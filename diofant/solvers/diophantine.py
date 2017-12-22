sympy/sympy

diop: transformation_to_DN

sympy/solvers/diophantine.py
@@ -1465,7 +1465,7 @@ def transformation_to_DN(eq):
     will give an equation of the form `x^2 - Dy^2 = N`.
 
     >>> from sympy.abc import X, Y
-    >>> from sympy import Matrix, simplify, Subs
+    >>> from sympy import Matrix, simplify
     >>> u = (A*Matrix([X, Y]) + B)[0] # Transformation for x
     >>> u
     X/26 + 3*Y/26 - 6/13
@@ -1476,7 +1476,7 @@ def transformation_to_DN(eq):
     Next we will substitute these formulas for `x` and `y` and do
     ``simplify()``.
 
-    >>> eq = simplify(Subs(x**2 - 3*x*y - y**2 - 2*y + 1, (x, y), (u, v)).doit())
+    >>> eq = simplify((x**2 - 3*x*y - y**2 - 2*y + 1).subs(zip((x, y), (u, v))))
     >>> eq
     X**2/676 - Y**2/52 + 17/13
 
@@ -1501,7 +1501,6 @@ def transformation_to_DN(eq):
            http://www.jpr2718.org/ax2p.pdf
     """
 
-
     var, coeff, diop_type = classify_diop(eq)
     if diop_type == "binary_quadratic":
         return _transformation_to_DN(var, coeff)
@@ -1509,57 +1508,45 @@ def transformation_to_DN(eq):
 
 def _transformation_to_DN(var, coeff):
 
-    x, y = var[:2]
+    x, y = var
 
     a = coeff[x**2]
     b = coeff[x*y]
     c = coeff[y**2]
     d = coeff[x]
     e = coeff[y]
-    f = coeff[Integer(1)]
+    f = coeff[1]
 
-    g = igcd(a, igcd(b, igcd(c, igcd(d, igcd(e, f)))))
-    a = a // g
-    b = b // g
-    c = c // g
-    d = d // g
-    e = e // g
-    f = f // g
+    a, b, c, d, e, f = [as_int(i) for i in _remove_gcd(a, b, c, d, e, f)]
 
     X, Y = symbols("X, Y", integer=True)
 
-    if b != Integer(0):
-        B = (S(2*a)/b).p
-        C = (S(2*a)/b).q
-        A = (S(a)/B**2).p
-        T = (S(a)/B**2).q
+    if b:
+        B, C = _rational_pq(2*a, b)
+        A, T = _rational_pq(a, B**2)
 
         # eq_1 = A*B*X**2 + B*(c*T - A*C**2)*Y**2 + d*T*X + (B*e*T - d*T*C)*Y + f*T*B
-        coeff = {X**2: A*B, X*Y: 0, Y**2: B*(c*T - A*C**2), X: d*T, Y: B*e*T - d*T*C, Integer(1): f*T*B}
+        coeff = {X**2: A*B, X*Y: 0, Y**2: B*(c*T - A*C**2), X: d*T, Y: B*e*T - d*T*C, 1: f*T*B}
         A_0, B_0 = _transformation_to_DN([X, Y], coeff)
         return Matrix(2, 2, [S(1)/B, -S(C)/B, 0, 1])*A_0, Matrix(2, 2, [S(1)/B, -S(C)/B, 0, 1])*B_0
 
     else:
-        if d != Integer(0):
-            B = (S(2*a)/d).p
-            C = (S(2*a)/d).q
-            A = (S(a)/B**2).p
-            T = (S(a)/B**2).q
+        if d:
+            B, C = _rational_pq(2*a, d)
+            A, T = _rational_pq(a, B**2)
 
             # eq_2 = A*X**2 + c*T*Y**2 + e*T*Y + f*T - A*C**2
-            coeff = {X**2: A, X*Y: 0, Y**2: c*T, X: 0, Y: e*T, Integer(1): f*T - A*C**2}
+            coeff = {X**2: A, X*Y: 0, Y**2: c*T, X: 0, Y: e*T, 1: f*T - A*C**2}
             A_0, B_0 = _transformation_to_DN([X, Y], coeff)
             return Matrix(2, 2, [S(1)/B, 0, 0, 1])*A_0, Matrix(2, 2, [S(1)/B, 0, 0, 1])*B_0 + Matrix([-S(C)/B, 0])
 
         else:
-            if e != Integer(0):
-                B = (S(2*c)/e).p
-                C = (S(2*c)/e).q
-                A = (S(c)/B**2).p
-                T = (S(c)/B**2).q
+            if e:
+                B, C = _rational_pq(2*c, e)
+                A, T = _rational_pq(c, B**2)
 
                 # eq_3 = a*T*X**2 + A*Y**2 + f*T - A*C**2
-                coeff = {X**2: a*T, X*Y: 0, Y**2: A, X: 0, Y: 0, Integer(1): f*T - A*C**2}
+                coeff = {X**2: a*T, X*Y: 0, Y**2: A, X: 0, Y: 0, 1: f*T - A*C**2}
                 A_0, B_0 = _transformation_to_DN([X, Y], coeff)
                 return Matrix(2, 2, [1, 0, 0, S(1)/B])*A_0, Matrix(2, 2, [1, 0, 0, S(1)/B])*B_0 + Matrix([0, -S(C)/B])
 

diofant/diofant

diofant/solvers/diophantine.py
@@ -1419,7 +1419,7 @@ def transformation_to_DN(eq):
     will give an equation of the form `x^2 - Dy^2 = N`.
 
     >>> from diofant.abc import X, Y
-    >>> from diofant import Matrix, simplify, Subs
+    >>> from diofant import Matrix, simplify
     >>> u = (A*Matrix([X, Y]) + B)[0]  # Transformation for x
     >>> u
     X/26 + 3*Y/26 - 6/13
@@ -1430,7 +1430,7 @@ def transformation_to_DN(eq):
     Next we will substitute these formulas for `x` and `y` and do
     ``simplify()``.
 
-    >>> eq = simplify(Subs(x**2 - 3*x*y - y**2 - 2*y + 1, (x, y), (u, v)).doit())
+    >>> eq = simplify((x**2 - 3*x*y - y**2 - 2*y + 1).subs({x: u, y: v}))
     >>> eq
     X**2/676 - Y**2/52 + 17/13
 
@@ -1462,51 +1462,45 @@ def transformation_to_DN(eq):
 
 def _transformation_to_DN(var, coeff):
 
-    x, y = var[:2]
+    x, y = var
 
     a = coeff[x**2]
     b = coeff[x*y]
     c = coeff[y**2]
     d = coeff[x]
     e = coeff[y]
-    f = coeff[Integer(1)]
+    f = coeff[1]
 
-    g = igcd(a, igcd(b, igcd(c, igcd(d, igcd(e, f)))))
-    a = a // g
-    b = b // g
-    c = c // g
-    d = d // g
-    e = e // g
-    f = f // g
+    a, b, c, d, e, f = [as_int(i) for i in _remove_gcd(a, b, c, d, e, f)]
 
     X, Y = symbols("X, Y", integer=True)
 
-    if b != Integer(0):
-        B, C = Rational(2*a, b).as_numer_denom()
-        A, T = Rational(a, B**2).as_numer_denom()
+    if b:
+        B, C = _rational_pq(2*a, b)
+        A, T = _rational_pq(a, B**2)
 
         # eq_1 = A*B*X**2 + B*(c*T - A*C**2)*Y**2 + d*T*X + (B*e*T - d*T*C)*Y + f*T*B
-        coeff = {X**2: A*B, X*Y: 0, Y**2: B*(c*T - A*C**2), X: d*T, Y: B*e*T - d*T*C, Integer(1): f*T*B}
+        coeff = {X**2: A*B, X*Y: 0, Y**2: B*(c*T - A*C**2), X: d*T, Y: B*e*T - d*T*C, 1: f*T*B}
         A_0, B_0 = _transformation_to_DN([X, Y], coeff)
         return Matrix(2, 2, [Integer(1)/B, -C/B, 0, 1])*A_0, Matrix(2, 2, [Integer(1)/B, -C/B, 0, 1])*B_0
 
     else:
-        if d != Integer(0):
-            B, C = Rational(2*a, d).as_numer_denom()
-            A, T = Rational(a, B**2).as_numer_denom()
+        if d:
+            B, C = _rational_pq(2*a, d)
+            A, T = _rational_pq(a, B**2)
 
             # eq_2 = A*X**2 + c*T*Y**2 + e*T*Y + f*T - A*C**2
-            coeff = {X**2: A, X*Y: 0, Y**2: c*T, X: 0, Y: e*T, Integer(1): f*T - A*C**2}
+            coeff = {X**2: A, X*Y: 0, Y**2: c*T, X: 0, Y: e*T, 1: f*T - A*C**2}
             A_0, B_0 = _transformation_to_DN([X, Y], coeff)
             return Matrix(2, 2, [Integer(1)/B, 0, 0, 1])*A_0, Matrix(2, 2, [Integer(1)/B, 0, 0, 1])*B_0 + Matrix([-C/B, 0])
 
         else:
-            if e != Integer(0):
-                B, C = Rational(2*c, e).as_numer_denom()
-                A, T = Rational(c, B**2).as_numer_denom()
+            if e:
+                B, C = _rational_pq(2*c, e)
+                A, T = _rational_pq(c, B**2)
 
                 # eq_3 = a*T*X**2 + A*Y**2 + f*T - A*C**2
-                coeff = {X**2: a*T, X*Y: 0, Y**2: A, X: 0, Y: 0, Integer(1): f*T - A*C**2}
+                coeff = {X**2: a*T, X*Y: 0, Y**2: A, X: 0, Y: 0, 1: f*T - A*C**2}
                 A_0, B_0 = _transformation_to_DN([X, Y], coeff)
                 return Matrix(2, 2, [1, 0, 0, Integer(1)/B])*A_0, Matrix(2, 2, [1, 0, 0, Integer(1)/B])*B_0 + Matrix([0, -C/B])
 