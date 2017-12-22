sympy/sympy

iterables: add ordered_partitions; use in diophantine

sympy/solvers/diophantine.py
@@ -2682,31 +2682,28 @@ def partition(n, k=None, zeros=False):
     >>> from sympy.solvers.diophantine import partition
     >>> f = partition(5)
     >>> next(f)
-    (5,)
+    (1, 1, 1, 1, 1)
     >>> next(f)
-    (1, 4)
+    (1, 1, 1, 2)
     >>> g = partition(5, 3)
     >>> next(g)
     (1, 1, 3)
     >>> next(g)
     (1, 2, 2)
+    >>> g = partition(5, 3, zeros=True)
+    >>> next(g)
+    (0, 0, 5)
 
-    Reference
-    =========
-
-    .. [1] Generating Integer Partitions, [online],
-        Available: http://jeromekelleher.net/partitions.php
     """
-    from sympy.utilities.iterables import partitions
-    for s, p in partitions(n, k, size=True):
-        if k and not zeros and s != k:
-            continue
-        sol = ()
-        for e in p:
-            sol += (e,)*p[e]
-        if zeros:
-            sol += (0,)*(k - s)
-        yield _sorted_tuple(*sol)
+    from sympy.utilities.iterables import ordered_partitions
+    if not zeros or k is None:
+        for i in ordered_partitions(n, k):
+            yield tuple(i)
+    else:
+        for m in range(1, k + 1):
+            for i in ordered_partitions(n, m):
+                i = tuple(i)
+                yield (0,)*(k - len(i)) + i
 
 
 def prime_as_sum_of_two_squares(p):

sympy/utilities/tests/test_iterables.py
@@ -15,7 +15,7 @@ from sympy.utilities.iterables import (
     multiset_permutations, necklaces, numbered_symbols, ordered, partitions,
     permutations, postfixes, postorder_traversal, prefixes, reshape,
     rotate_left, rotate_right, runs, sift, subsets, take, topological_sort,
-    unflatten, uniq, variations)
+    unflatten, uniq, variations, ordered_partitions)
 from sympy.utilities.enumerative import (
     factoring_visitor, multiset_partitions_taocp )
 
@@ -709,3 +709,16 @@ def test__partition():
         ['b', 'e'], ['a', 'c'], ['d']]
     output = (3, [1, 0, 1, 2, 0])
     assert _partition('abcde', *output) == [['b', 'e'], ['a', 'c'], ['d']]
+
+
+def test_ordered_partitions():
+    from sympy.functions.combinatorial.numbers import nT
+    f = ordered_partitions
+    raises(ValueError, lambda: list(f(0, 1)))
+    raises(ValueError, lambda: list(f(1, 0)))
+    for i in range(1, 7):
+        for j in [None] + list(range(1, i)):
+            assert (
+                sum(1 for p in f(i, j, 1)) ==
+                sum(1 for p in f(i, j, 0)) ==
+                nT(i, j))

diofant/diofant

diofant/solvers/diophantine.py
@@ -2609,31 +2609,27 @@ def partition(n, k=None, zeros=False):
     >>> from diofant.solvers.diophantine import partition
     >>> f = partition(5)
     >>> next(f)
-    (5,)
+    (1, 1, 1, 1, 1)
     >>> next(f)
-    (1, 4)
+    (1, 1, 1, 2)
     >>> g = partition(5, 3)
     >>> next(g)
     (1, 1, 3)
     >>> next(g)
     (1, 2, 2)
-
-    References
-    ==========
-
-    .. [1] Generating Integer Partitions, [online],
-        Available: https://web.archive.org/web/20151103102545/http://jeromekelleher.net:80/partitions.php
+    >>> g = partition(5, 3, zeros=True)
+    >>> next(g)
+    (0, 0, 5)
     """
-    from diofant.utilities.iterables import partitions
-    for s, p in partitions(n, k, size=True):
-        if k and not zeros and s != k:
-            continue
-        sol = ()
-        for e in p:
-            sol += (e,)*p[e]
-        if zeros:
-            sol += (0,)*(k - s)
-        yield _sorted_tuple(*sol)
+    from diofant.utilities.iterables import ordered_partitions
+    if not zeros or k is None:
+        for i in ordered_partitions(n, k):
+            yield tuple(i)
+    else:
+        for m in range(1, k + 1):
+            for i in ordered_partitions(n, m):
+                i = tuple(i)
+                yield (0,)*(k - len(i)) + i
 
 
 def prime_as_sum_of_two_squares(p):

diofant/utilities/tests/test_iterables.py
@@ -9,6 +9,7 @@ from diofant import (Basic, Dummy, Integer, Integral, Matrix, Tuple,
                      default_sort_key, factorial, symbols, true)
 from diofant.abc import w, x, y, z
 from diofant.combinatorics import Permutation, RGS_enum, RGS_unrank
+from diofant.functions.combinatorial.numbers import nT
 from diofant.functions.elementary.piecewise import ExprCondPair, Piecewise
 from diofant.utilities.enumerative import (factoring_visitor,
                                            multiset_partitions_taocp)
@@ -24,7 +25,8 @@ from diofant.utilities.iterables import (_partition, _set_partitions,
                                          multiset_combinations,
                                          multiset_partitions,
                                          multiset_permutations, necklaces,
-                                         numbered_symbols, ordered, partitions,
+                                         numbered_symbols, ordered,
+                                         ordered_partitions, partitions,
                                          permutations, postfixes,
                                          postorder_traversal, prefixes,
                                          reshape, rotate_left, rotate_right,
@@ -754,3 +756,13 @@ def test_cantor_product():
     assert (list(itertools.islice(cantor_product(itertools.count(1),
                                                  itertools.count(1)), 7)) ==
             [(1, 1), (1, 2), (2, 1), (2, 2), (1, 3), (2, 3), (3, 1)])
+
+
+def test_ordered_partitions():
+    pytest.raises(ValueError, lambda: list(ordered_partitions(0, 1)))
+    pytest.raises(ValueError, lambda: list(ordered_partitions(1, 0)))
+    for i in range(1, 7):
+        for j in [None] + list(range(1, i)):
+            assert (sum(1 for p in ordered_partitions(i, j, 1)) ==
+                    sum(1 for p in ordered_partitions(i, j, 0)) ==
+                    nT(i, j))