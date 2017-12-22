sympy/sympy

ordered_partitions returns [] for emptyset result

sympy/utilities/tests/test_iterables.py
@@ -718,8 +718,8 @@ def test__partition():
 def test_ordered_partitions():
     from sympy.functions.combinatorial.numbers import nT
     f = ordered_partitions
-    raises(ValueError, lambda: list(f(0, 1)))
-    raises(ValueError, lambda: list(f(1, 0)))
+    assert list(f(0, 1)) == [[]]
+    assert list(f(1, 0)) == [[]]
     for i in range(1, 7):
         for j in [None] + list(range(1, i)):
             assert (

diofant/diofant

diofant/utilities/tests/test_iterables.py
@@ -758,8 +758,8 @@ def test_cantor_product():
 
 
 def test_ordered_partitions():
-    pytest.raises(ValueError, lambda: list(ordered_partitions(0, 1)))
-    pytest.raises(ValueError, lambda: list(ordered_partitions(1, 0)))
+    assert list(ordered_partitions(0, 1)) == [[]]
+    assert list(ordered_partitions(1, 0)) == [[]]
     for i in range(1, 7):
         for j in [None] + list(range(1, i)):
             assert (sum(1 for p in ordered_partitions(i, j, 1)) ==