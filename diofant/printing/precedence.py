sympy/sympy

printing: Added a new precedence 'Func'
This precedence is used by the parenthesize function, to find where to
place brackets for functions like factorial.
Earlier 'Pow' precedence was used for these functions, which behaved
exactly same, but was technically incorrect.

sympy/printing/precedence.py
@@ -14,6 +14,7 @@ PRECEDENCE = {
     "Add": 40,
     "Mul": 50,
     "Pow": 60,
+    "Func": 70,
     "Not": 100,
     "Atom": 1000
 }
@@ -32,8 +33,7 @@ PRECEDENCE_VALUES = {
     "Relational": PRECEDENCE["Relational"],
     "Sub": PRECEDENCE["Add"],
     "Not": PRECEDENCE["Not"],
-    "factorial": PRECEDENCE["Pow"],
-    "factorial2": PRECEDENCE["Pow"],
+    "Function" : PRECEDENCE["Func"],
     "NegativeInfinity": PRECEDENCE["Add"],
     "MatAdd": PRECEDENCE["Add"],
     "MatMul": PRECEDENCE["Mul"],

diofant/diofant

Signed-off-by: Sergey B Kirpichev <skirpichev@gmail.com>

diofant/printing/precedence.py
@@ -14,6 +14,7 @@ PRECEDENCE = {
     "Mul": 50,
     "Mod": 50,
     "Pow": 60,
+    "Func": 70,
     "Not": 100,
     "Atom": 1000
 }
@@ -33,8 +34,9 @@ PRECEDENCE_VALUES = {
     "Relational": PRECEDENCE["Relational"],
     "Sub": PRECEDENCE["Add"],
     "Not": PRECEDENCE["Not"],
-    "factorial": PRECEDENCE["Pow"],
-    "factorial2": PRECEDENCE["Pow"],
+    "factorial": PRECEDENCE["Func"],
+    "factorial2": PRECEDENCE["Func"],
+    "Function": PRECEDENCE["Func"],
     "NegativeInfinity": PRECEDENCE["Add"],
     "MatAdd": PRECEDENCE["Add"],
     "MatMul": PRECEDENCE["Mul"],