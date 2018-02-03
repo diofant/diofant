0eda389f39ac1d080e67d695810bdc3bc6bf4b1a
Sergey B Kirpichev <skirpichev@gmail.com>
2018-02-03 23:55:39 +0300

    0.9.0b1

--- diofant/__init__.py
+++ diofant/__init__.py
@@ -1,6 +1,6 @@
 """Diofant is a Python library for symbolic mathematics. """
 
-__version__ = "0.9.0a4"
+__version__ = "0.9.0b1"
 
 import os
 DIOFANT_DEBUG = os.getenv('DIOFANT_DEBUG', 'False') != 'False'
