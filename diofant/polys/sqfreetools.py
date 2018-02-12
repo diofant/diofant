63abffde7bc56feb93ad21931ea61fce0dd9c09d
Sergey B Kirpichev <skirpichev@gmail.com>
2018-02-12 21:22:11 +0300

    polys: reorder functions in sqfreetools.py

--- diofant/polys/sqfreetools.py
+++ diofant/polys/sqfreetools.py
@@ -200,35 +200,6 @@ def dup_sqf_list(f, K, all=False):
     return coeff, result
 
 
-def dup_sqf_list_include(f, K, all=False):
-    """
-    Return square-free decomposition of a polynomial in ``K[x]``.
-
-    Examples
-    ========
-
-    >>> from diofant.domains import ZZ
-    >>> from diofant.polys import ring
-    >>> R, x = ring("x", ZZ)
-
-    >>> f = 2*x**5 + 16*x**4 + 50*x**3 + 76*x**2 + 56*x + 16
-
-    >>> R.dup_sqf_list_include(f)
-    [(2, 1), (x + 1, 2), (x + 2, 3)]
-    >>> R.dup_sqf_list_include(f, all=True)
-    [(2, 1), (x + 1, 2), (x + 2, 3)]
-
-    """
-    coeff, factors = dup_sqf_list(f, K, all=all)
-
-    if factors and factors[0][1] == 1:
-        g = dmp_mul_ground(factors[0][0], coeff, 0, K)
-        return [(g, 1)] + factors[1:]
-    else:
-        g = dmp_strip([coeff], 0)
-        return [(g, 1)] + factors
-
-
 def dmp_sqf_list(f, u, K, all=False):
     """
     Return square-free decomposition of a polynomial in ``K[X]``.
@@ -288,6 +259,35 @@ def dmp_sqf_list(f, u, K, all=False):
         i += 1
 
     return coeff, result
+
+
+def dup_sqf_list_include(f, K, all=False):
+    """
+    Return square-free decomposition of a polynomial in ``K[x]``.
+
+    Examples
+    ========
+
+    >>> from diofant.domains import ZZ
+    >>> from diofant.polys import ring
+    >>> R, x = ring("x", ZZ)
+
+    >>> f = 2*x**5 + 16*x**4 + 50*x**3 + 76*x**2 + 56*x + 16
+
+    >>> R.dup_sqf_list_include(f)
+    [(2, 1), (x + 1, 2), (x + 2, 3)]
+    >>> R.dup_sqf_list_include(f, all=True)
+    [(2, 1), (x + 1, 2), (x + 2, 3)]
+
+    """
+    coeff, factors = dup_sqf_list(f, K, all=all)
+
+    if factors and factors[0][1] == 1:
+        g = dmp_mul_ground(factors[0][0], coeff, 0, K)
+        return [(g, 1)] + factors[1:]
+    else:
+        g = dmp_strip([coeff], 0)
+        return [(g, 1)] + factors
 
 
 def dmp_sqf_list_include(f, u, K, all=False):
