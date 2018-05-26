b71248d9ad19938ae908e88723910045ce519906
Sergey B Kirpichev <skirpichev@gmail.com>
2018-05-26 11:42:47 +0300

    polys: remove redundant (for coverage) test in test_RootOf_algebraic_domain()

--- diofant/polys/tests/test_rootoftools.py
+++ diofant/polys/tests/test_rootoftools.py
@@ -542,8 +542,3 @@ def test_RootOf_algebraic_domain():
     e = RootOf(x**4 + t*x + 1, 1, extension=True)
     assert e == RootOf(x**20 + 5*x**16 + 10*x**12 + 14*x**8 -
                        2*x**5 + 9*x**4 + 1, 5)
-
-    e = RootOf(x**3 - root(2, 3)*x + 5*root(-2, 3)**5, 0, extension=True)
-    assert e == RootOf(x**18 + 60*x**13 - 4*x**12 - 8000*x**9 +
-                       3600*x**8 - 120*x**7 + 4*x**6 -
-                       240000*x**4 + 16000*x**3 + 16000000, 1)
