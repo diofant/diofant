97294ac8a861b8f422e098e264bb85ac65c0f2f0
Sergey B Kirpichev <skirpichev@gmail.com>
2018-07-15 21:07:19 +0300

    Amend dc3ff6f60e

--- diofant/polys/rootisolation.py
+++ diofant/polys/rootisolation.py
@@ -1166,7 +1166,7 @@ def _reverse_intervals(intervals):
 
 def _winding_number(T, field):
     """Compute the winding number of the input polynomial, i.e. the number of roots. """
-    return int(sum(field(*_values[t][i]) for t, i in T) / field(2))
+    return int(sum((field(_values[t][i][0])/field(_values[t][i][1]) for t, i in T), field(0)) / field(2))
 
 
 def _roots_bound(f, F):
