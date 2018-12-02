7f51770b15a2e6edcd349081c883cc5190e9788c
Sergey B Kirpichev <skirpichev@gmail.com>
2018-12-02 18:10:01 +0300

    Use setuptools_scm to handle package versions

--- setup.py
+++ setup.py
@@ -5,16 +5,7 @@ Setup script for Diofant.
 This script uses Setuptools (https://setuptools.readthedocs.io/en/latest/).
 """
 
-import re
-
 import setuptools
 
 
-with open('diofant/__init__.py') as f:
-    m = re.search('^__version__ = "([0-9ab.]+(dev[0-9]+)?)".*$', f.read(), re.M)
-    if m:
-        __version__ = m.group(1)
-    else:
-        raise RuntimeError("Unable to find version string.")
-
-setuptools.setup(version=__version__)
+setuptools.setup(use_scm_version=True)
