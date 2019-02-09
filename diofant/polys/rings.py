04266e647e69ee29f7238141ac23a6f2c40c095e
Sergey B Kirpichev <skirpichev@gmail.com>
2019-02-09 15:05:14 +0300

    polys: drop PolyElement._gcd_monom() helper to be more compatible with DMP
    Also,
    
    * removed duplicated test for hermite_reduce(), which was introduced by
      60e669d2a.  It's a copy-paste of the test right at the end of the
      test function.
    
    * normalize q, returned by weak_normalizer(), to be a monic polynomial.
      This fix test_weak_normalizer(), which would be broken after gcd's
      changes in this commit.  Thanks to @jksuom for suggestion.
    
    * slightly changed two test in test_hermite_reduce(), which were reverted
      to state before 8d89dada6.

--- diofant/polys/rings.py
+++ diofant/polys/rings.py
@@ -20,8 +20,8 @@ from .constructor import construct_domain
 from .densebasic import dmp_from_dict, dmp_to_dict
 from .heuristicgcd import heugcd
 from .modulargcd import func_field_modgcd, modgcd
-from .monomials import (monomial_div, monomial_gcd, monomial_ldiv,
-                        monomial_min, monomial_mul, monomial_pow)
+from .monomials import (monomial_div, monomial_ldiv, monomial_min,
+                        monomial_mul, monomial_pow)
 from .orderings import lex
 from .polyconfig import query
 from .polyerrors import (CoercionFailed, ExactQuotientFailed, GeneratorsError,
@@ -1942,12 +1942,6 @@ class PolyElement(DomainElement, CantSympify, dict):
         elif not other:
             h, cfg, cff = other._gcd_zero(self)
             return h, cff, cfg
-        elif len(self) == 1:
-            h, cff, cfg = self._gcd_monom(other)
-            return h, cff, cfg
-        elif len(other) == 1:
-            h, cfg, cff = other._gcd_monom(self)
-            return h, cff, cfg
 
         J, (f, g) = self.deflate(other)
         h, cff, cfg = f._gcd(g)
@@ -1960,20 +1954,6 @@ class PolyElement(DomainElement, CantSympify, dict):
             return other, zero, one
         else:
             return -other, zero, -one
-
-    def _gcd_monom(self, other):
-        ring = self.ring
-        ground_gcd = ring.domain.gcd
-        ground_quo = ring.domain.quo
-        mf, cf = list(self.items())[0]
-        _mgcd, _cgcd = mf, cf
-        for mg, cg in other.items():
-            _mgcd = monomial_gcd(_mgcd, mg)
-            _cgcd = ground_gcd(_cgcd, cg)
-        h = self.new([(_mgcd, _cgcd)])
-        cff = self.new([(monomial_ldiv(mf, _mgcd), ground_quo(cf, _cgcd))])
-        cfg = self.new([(monomial_ldiv(mg, _mgcd), ground_quo(cg, _cgcd)) for mg, cg in other.items()])
-        return h, cff, cfg
 
     def _gcd(self, other):
         ring = self.ring
