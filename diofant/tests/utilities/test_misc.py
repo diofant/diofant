6482b24da17492f9e9eb210eaa107b0dcfa121da
Sergey B Kirpichev <skirpichev@gmail.com>
2019-10-29 12:17:36 +0300

    100% test coverage for decorator.py

--- diofant/tests/utilities/test_misc.py
+++ diofant/tests/utilities/test_misc.py
@@ -1,4 +1,5 @@
 from diofant.tests.core.test_cache import clear_imports  # noqa: F401
+from diofant.utilities.decorator import no_attrs_in_subclass
 from diofant.utilities.misc import debug
 
 
@@ -17,3 +18,16 @@ def test_debug_off(capsys):
     debug('Hi')
 
     assert capsys.readouterr().err == ''
+
+
+def test_no_attrs_in_subclass():
+    class A:
+        x = 'test'
+
+    A.x = no_attrs_in_subclass(A, A.x)
+
+    class B(A):
+        pass
+
+    assert hasattr(A, 'x') is True
+    assert hasattr(B, 'x') is False
