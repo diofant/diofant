981480871a83a3eae87714ccb9b9f3a0ef0c5890
Sergey B Kirpichev <skirpichev@gmail.com>
2020-06-23 10:28:37 +0300

    polys: drop unused dmp_resultant() wrapper

--- diofant/polys/euclidtools.py
+++ diofant/polys/euclidtools.py
@@ -1,19 +1,9 @@
 """Euclidean algorithms, GCDs, LCMs and polynomial remainder sequences."""
 
-from ..core import cacheit
 from ..ntheory import nextprime
 from ..ntheory.modular import crt, symmetric_residue
 from .polyconfig import query
 from .polyerrors import DomainError, HeuristicGCDFailed, HomomorphismFailed
-
-
-@cacheit
-def dmp_resultant(f, g, u, K):
-    """Computes resultant of two polynomials in `K[X]`."""
-    ring = K.poly_ring(*[f'_{i}' for i in range(u + 1)])
-    f, g = map(ring.from_list, (f, g))
-    res = f.resultant(g)
-    return res.to_dense()
 
 
 def dmp_gcd(f, g, u, K):
