8ceba9543fb8e2b6c355cdb53282880e8746cfd5
Sergey B Kirpichev <skirpichev@gmail.com>
2020-08-27 00:08:20 +0300

    series: correct formatted string

--- diofant/series/limits.py
+++ diofant/series/limits.py
@@ -136,7 +136,7 @@ class Limit(Expr):
             left = limit(e, z, z0, '-')
             if not (left - right).equals(0):
                 raise PoleError(f'left and right limits for expression {e} at '
-                                'point {z}={z0} seems to be not equal')
+                                f'point {z}={z0} seems to be not equal')
             else:
                 return right
 
