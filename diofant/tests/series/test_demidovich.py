7e91e89a82a972f3deea12b6364f7485d81afde7
Sergey B Kirpichev <skirpichev@gmail.com>
2021-09-12 13:51:08 +0300

    Simplify, reformat test_demidovich.py

--- diofant/tests/series/test_demidovich.py
+++ diofant/tests/series/test_demidovich.py
@@ -3,19 +3,11 @@ Numbers listed with the tests refer to problem numbers in the book
 "Anti-demidovich, problemas resueltos, Ed. URSS"
 """
 from diofant import (Rational, asin, cos, exp, limit, log, oo, pi, root, sin,
-                     sqrt, tan, together)
+                     sqrt, tan)
 from diofant.abc import a, h, m, n, x
 
 
 __all__ = ()
-
-
-def root3(x):
-    return root(x, 3)
-
-
-def root4(x):
-    return root(x, 4)
 
 
 def test_Limits_simple_0():
@@ -26,7 +18,7 @@ def test_Limits_simple_1():
     assert limit((x + 1)*(x + 2)*(x + 3)/x**3, x, oo) == 1  # 172
     assert limit(sqrt(x + 1) - sqrt(x), x, oo) == 0  # 179
     assert limit((2*x - 3)*(3*x + 5)*(4*x - 6)/(3*x**3 + x - 1), x, oo) == 8  # Primjer 1
-    assert limit(x/root3(x**3 + 10), x, oo) == 1  # Primjer 2
+    assert limit(x/root(x**3 + 10, 3), x, oo) == 1  # Primjer 2
     assert limit((x + 1)**2/(x**2 + 1), x, oo) == 1  # 181
 
 
@@ -35,32 +27,34 @@ def test_Limits_simple_2():
     assert limit((x**2 - 5*x + 1)/(3*x + 7), x, oo) == oo  # 183
     assert limit((2*x**2 - x + 3)/(x**3 - 8*x + 5), x, oo) == 0  # 184
     assert limit((2*x**2 - 3*x - 4)/sqrt(x**4 + 1), x, oo) == 2  # 186
-    assert limit((2*x + 3)/(x + root3(x)), x, oo) == 2  # 187
+    assert limit((2*x + 3)/(x + root(x, 3)), x, oo) == 2  # 187
     assert limit(x**2/(10 + x*sqrt(x)), x, oo) == oo  # 188
-    assert limit(root3(x**2 + 1)/(x + 1), x, oo) == 0  # 189
+    assert limit(root(x**2 + 1, 3)/(x + 1), x, oo) == 0  # 189
     assert limit(sqrt(x)/sqrt(x + sqrt(x + sqrt(x))), x, oo) == 1  # 190
 
 
 def test_Limits_simple_3a():
     # issue sympy/sympy#3513
-    assert together(limit((x**2 - (a + 1)*x + a)/(x**3 - a**3), x, a)) == \
-        (a - 1)/(3*a**2)  # 196
+    assert limit((x**2 - (a + 1)*x + a)/(x**3 - a**3),
+                 x, a) == (a - 1)/(3*a**2)  # 196
 
 
 def test_Limits_simple_3b():
     assert limit(((x + h)**3 - x**3)/h, h, 0) == 3*x**2  # 197
-    assert limit((1/(1 - x) - 3/(1 - x**3)), x, 1) == -1  # 198
-    assert limit((sqrt(1 + x) - 1)/(root3(1 + x) - 1), x, 0) == Rational(3, 2)  # Primer 4
+    assert limit(1/(1 - x) - 3/(1 - x**3), x, 1) == -1  # 198
+    assert limit((sqrt(1 + x) - 1)/(root(1 + x, 3) - 1),
+                 x, 0) == Rational(3, 2)  # Primer 4
     assert limit((sqrt(x) - 1)/(x - 1), x, 1) == Rational(1, 2)  # 199
-    assert limit((sqrt(x) - 8)/(root3(x) - 4), x, 64) == 3  # 200
-    assert limit((root3(x) - 1)/(root4(x) - 1), x, 1) == Rational(4, 3)  # 201
-    assert limit(
-        (root3(x**2) - 2*root3(x) + 1)/(x - 1)**2, x, 1) == Rational(1, 9)  # 202
+    assert limit((sqrt(x) - 8)/(root(x, 3) - 4), x, 64) == 3  # 200
+    assert limit((root(x, 3) - 1)/(root(x, 4) - 1),
+                 x, 1) == Rational(4, 3)  # 201
+    assert limit((root(x**2, 3) - 2*root(x, 3) + 1)/(x - 1)**2,
+                 x, 1) == Rational(1, 9)  # 202
 
 
 def test_Limits_simple_4a():
     assert limit((sqrt(x) - sqrt(a))/(x - a), x, a) == 1/(2*sqrt(a))  # Primer 5
-    assert limit((sqrt(x) - 1)/(root3(x) - 1), x, 1) == Rational(3, 2)  # 205
+    assert limit((sqrt(x) - 1)/(root(x, 3) - 1), x, 1) == Rational(3, 2)  # 205
     assert limit((sqrt(1 + x) - sqrt(1 - x))/x, x, 0) == 1  # 207
     assert limit(sqrt(x**2 - 5*x + 6) - x, x, oo) == -Rational(5, 2)  # 213
 
@@ -71,7 +65,7 @@ def test_limits_simple_4aa():
 
 def test_Limits_simple_4b():
     # issue sympy/sympy#3511
-    assert limit(x - root3(x**3 - 1), x, oo) == 0  # 215
+    assert limit(x - root(x**3 - 1, 3), x, oo) == 0  # 215
 
 
 def test_Limits_simple_4c():
@@ -119,8 +113,8 @@ def test_f2a():
 
 
 def test_f2():
-    assert limit((sqrt(
-        cos(x)) - root3(cos(x)))/(sin(x)**2), x, 0) == -Rational(1, 12)  # *184
+    assert limit((sqrt(cos(x)) - root(cos(x), 3))/sin(x)**2,
+                 x, 0) == -Rational(1, 12)  # *184
 
 
 def test_f3():
