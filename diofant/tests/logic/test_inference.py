761ad00af9017ea95d690fb83c4e6effb1412791
Sergey B Kirpichev <skirpichev@gmail.com>
2022-03-16 12:07:51 +0300

    Amend dd466e3af7

--- diofant/tests/logic/test_inference.py
+++ diofant/tests/logic/test_inference.py
@@ -4,8 +4,7 @@ import copy
 
 import pytest
 
-from diofant import (And, Equivalent, Implies, Or, false, numbered_symbols, pi,
-                     satisfiable, true)
+from diofant import Equivalent, Or, Symbol, false, pi, satisfiable, true
 from diofant.abc import a, b, c, x, y
 from diofant.logic.algorithms.dpll import (dpll, find_pure_symbol,
                                            find_unit_clause, unit_propagate)
@@ -32,20 +31,19 @@ def test_find_pure_symbol():
 
 
 def test_unit_clause():
-    assert find_unit_clause(map(set, [[1]]), {}) == (1, True)
-    assert find_unit_clause(map(set, [[1], [-1]]), {}) == (1, True)
+    assert find_unit_clause([{1}], {}) == (1, True)
+    assert find_unit_clause([{1}, {-1}], {}) == (1, True)
     assert find_unit_clause([{1, 2}], {1: True}) == (2, True)
     assert find_unit_clause([{1, 2}], {2: True}) == (1, True)
-    assert find_unit_clause(map(set, [[1, 2, 3], [2, -3], [1, -2]]),
+    assert find_unit_clause([{1, 2, 3}, {2, -3}, {1, -2}],
                             {1: True}) == (2, False)
-    assert find_unit_clause(map(set, [[1, 2, 3], [3, -3], [1, 2]]),
+    assert find_unit_clause([{1, 2, 3}, {3, -3}, {1, 2}],
                             {1: True}) == (2, True)
 
 
 def test_unit_propagate():
     assert unit_propagate([{1, 2}], 1) == []
-    assert unit_propagate(map(set, [[1, 2], [-1, 3],
-                                    [-3, 2], [1]]), 1) == [{3}, {-3, 2}]
+    assert unit_propagate([{1, 2}, {3, -1}, {2, -3}, {1}], 1) == [{3}, {-3, 2}]
 
 
 def test_dpll():
@@ -63,10 +61,10 @@ def test_dpll2_satisfiable():
 
     l = copy.deepcopy(l0)
     assert l.num_learned_clauses == 0
-    assert l.lit_scores == {-3: -2.0, -2: -2.0, -1: 0.0, 1: 0.0, 2: -2.0, 3: -2.0}
+    assert l.lit_scores == {-3: -2, -2: -2, -1: 0, 1: 0, 2: -2, 3: -2}
     l._vsids_clause_added({2, -3})
     assert l.num_learned_clauses == 1
-    assert l.lit_scores == {-3: -1.0, -2: -2.0, -1: 0.0, 1: 0.0, 2: -1.0, 3: -2.0}
+    assert l.lit_scores == {-3: -1, -2: -2, -1: 0, 1: 0, 2: -1, 3: -2}
 
     l = copy.deepcopy(l0)
     assert l.num_learned_clauses == 0
@@ -77,9 +75,9 @@ def test_dpll2_satisfiable():
     assert l.sentinels == {-3: {0, 2}, -2: {3, 4}, 2: {0, 3}, 3: {2, 4, 5}}
 
     l = copy.deepcopy(l0)
-    assert l.lit_scores == {-3: -2.0, -2: -2.0, -1: 0.0, 1: 0.0, 2: -2.0, 3: -2.0}
+    assert l.lit_scores == {-3: -2, -2: -2, -1: 0, 1: 0, 2: -2, 3: -2}
     l._vsids_decay()
-    assert l.lit_scores == {-3: -1.0, -2: -1.0, -1: 0.0, 1: 0.0, 2: -1.0, 3: -1.0}
+    assert l.lit_scores == {-3: -1, -2: -1, -1: 0, 1: 0, 2: -1, 3: -1}
 
     l = copy.deepcopy(l0)
     assert next(l._find_model()) == {1: True, 2: False, 3: False}
@@ -104,21 +102,18 @@ def test_pl_true():
     assert pl_true(a | b | ~c, {a: False, b: True, c: True}) is True
     assert pl_true(Equivalent(a, b), {a: False, b: False}) is True
 
-    # test for false
     assert pl_true(False) is False
     assert pl_true(a & b, {a: False, b: False}) is False
     assert pl_true(a & b, {a: False}) is False
     assert pl_true(a & b, {b: False}) is False
     assert pl_true(a | b, {a: False, b: False}) is False
 
-    # test for None
     assert pl_true(b, {b: None}) is None
     assert pl_true(a & b, {a: True, b: None}) is None
     assert pl_true(a >> b, {a: True, b: None}) is None
     assert pl_true(Equivalent(a, b), {a: None}) is None
     assert pl_true(Equivalent(a, b), {a: True, b: None}) is None
 
-    # Test for deep
     assert pl_true(a | b, {a: False}, deep=True) is None
     assert pl_true(~a & ~b, {a: False}, deep=True) is None
     assert pl_true(a | b, {a: False, b: False}, deep=True) is False
@@ -126,7 +121,7 @@ def test_pl_true():
     assert pl_true((c >> a) >> (b >> a), {c: True}, deep=True) is True
 
     pytest.raises(ValueError, lambda: pl_true('John Cleese'))
-    pytest.raises(ValueError, lambda: pl_true(42 + pi + pi ** 2))
+    pytest.raises(ValueError, lambda: pl_true(42 + pi))
     pytest.raises(ValueError, lambda: pl_true(42))
 
 
@@ -139,12 +134,18 @@ def test_entails():
 
 def test_PropKB():
     kb = PropKB()
+
     assert not kb.clauses
+    assert kb.ask(b) is False
     assert kb.ask(a >> b) is False
     assert kb.ask(a >> (b >> a)) is True
+
     kb.tell(a >> b)
+
     assert kb.clauses == [~a | b]
+
     kb.tell(b >> c)
+
     assert kb.clauses == [~a | b, ~b | c]
     assert kb.ask(a) is False
     assert kb.ask(b) is False
@@ -153,18 +154,21 @@ def test_PropKB():
     assert kb.ask(~b) is False
     assert kb.ask(~c) is False
     assert kb.ask(a >> c) is True
+
     kb.tell(a)
+
     assert kb.ask(a) is True
     assert kb.ask(b) is True
     assert kb.ask(c) is True
     assert kb.ask(~c) is False
-    kb.retract(a)
-    assert kb.ask(c) is False
-    kb = PropKB((a >> b) & (b >> c))
-    assert kb.ask(a >> c) is True
 
-    kb = PropKB()
-    assert kb.ask(b) is False
+    kb.retract(a)
+
+    assert kb.ask(c) is False
+
+    kb = PropKB((a >> b) & (b >> c))
+
+    assert kb.ask(a >> c) is True
 
 
 @pytest.mark.parametrize('algorithm', ['dpll', 'dpll2'])
@@ -197,15 +201,15 @@ def test_satisfiable(algorithm):
         pass
 
     assumptions = Zero(x*y)
-    facts = Implies(Zero(x*y), Zero(x) | Zero(y))
+    facts = Zero(x*y) >> (Zero(x) | Zero(y))
     query = ~Zero(x) & ~Zero(y)
     refutations = [{Zero(x): True, Zero(x*y): True},
                    {Zero(y): True, Zero(x*y): True},
                    {Zero(x): True, Zero(y): True, Zero(x*y): True},
                    {Zero(x): True, Zero(y): False, Zero(x*y): True},
                    {Zero(x): False, Zero(y): True, Zero(x*y): True}]
-    assert not satisfiable(And(assumptions, facts, query), algorithm=algorithm)
-    assert satisfiable(And(assumptions, facts, ~query),
+    assert not satisfiable(assumptions & facts & query, algorithm=algorithm)
+    assert satisfiable(assumptions & facts & ~query,
                        algorithm=algorithm) in refutations
 
 
@@ -234,8 +238,7 @@ def test_satisfiable_all_models():
     # This is a santiy test to check that only the required number
     # of solutions are generated. The expr below has 2**100 - 1 models
     # which would time out the test if all are generated at once.
-    sym = numbered_symbols()
-    X = [next(sym) for i in range(100)]
+    X = [Symbol(f'x{i}') for i in range(100)]
     result = satisfiable(Or(*X), all_models=True)
     for _ in range(10):
         assert next(result)
